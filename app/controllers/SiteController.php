9e267f905b449d7bf094dfeab80f05944719aacd
Fredrik Wollsén <fredrik@neam.se>
2013-06-14 14:31:00 +0200

    Auto-formatting

--- app/controllers/SiteController.php
+++ app/controllers/SiteController.php
@@ -2,6 +2,7 @@
 
 class SiteController extends Controller
 {
+
 	/**
 	 * Declares class-based actions.
 	 */
@@ -9,14 +10,14 @@ class SiteController extends Controller
 	{
 		return array(
 			// captcha action renders the CAPTCHA image displayed on the contact page
-			'captcha'=>array(
-				'class'=>'CCaptchaAction',
-				'backColor'=>0xFFFFFF,
+			'captcha' => array(
+				'class' => 'CCaptchaAction',
+				'backColor' => 0xFFFFFF,
 			),
 			// page action renders "static" pages stored under 'protected/views/site/pages'
 			// They can be accessed via: index.php?r=site/page&view=FileName
-			'page'=>array(
-				'class'=>'CViewAction',
+			'page' => array(
+				'class' => 'CViewAction',
 			),
 		);
 	}
@@ -37,17 +38,18 @@ class SiteController extends Controller
 	 */
 	public function actionError()
 	{
-		if($error=Yii::app()->errorHandler->error)
+		if ($error = Yii::app()->errorHandler->error)
 		{
-			if(Yii::app()->request->isAjaxRequest)
+			if (Yii::app()->request->isAjaxRequest)
 				echo $error['message'];
 			else
 				$this->render('error', $error);
-		} else {
-            $error['message'] = "Precondition Failed";
-            $error['code'] = 412;
-            $this->render('error', $error);
-        }
+		} else
+		{
+			$error['message'] = "Precondition Failed";
+			$error['code'] = 412;
+			$this->render('error', $error);
+		}
 	}
 
 	/**
@@ -55,25 +57,25 @@ class SiteController extends Controller
 	 */
 	public function actionContact()
 	{
-		$model=new ContactForm;
-		if(isset($_POST['ContactForm']))
+		$model = new ContactForm;
+		if (isset($_POST['ContactForm']))
 		{
-			$model->attributes=$_POST['ContactForm'];
-			if($model->validate())
+			$model->attributes = $_POST['ContactForm'];
+			if ($model->validate())
 			{
-				$name='=?UTF-8?B?'.base64_encode($model->name).'?=';
-				$subject='=?UTF-8?B?'.base64_encode($model->subject).'?=';
-				$headers="From: $name <{$model->email}>\r\n".
-					"Reply-To: {$model->email}\r\n".
-					"MIME-Version: 1.0\r\n".
-					"Content-type: text/plain; charset=UTF-8";
+				$name = '=?UTF-8?B?' . base64_encode($model->name) . '?=';
+				$subject = '=?UTF-8?B?' . base64_encode($model->subject) . '?=';
+				$headers = "From: $name <{$model->email}>\r\n" .
+				    "Reply-To: {$model->email}\r\n" .
+				    "MIME-Version: 1.0\r\n" .
+				    "Content-type: text/plain; charset=UTF-8";
 
-				mail(Yii::app()->params['adminEmail'],$subject,$model->body,$headers);
-				Yii::app()->user->setFlash('contact',Yii::t('app', 'Thank you for contacting us. We will respond to you as soon as possible.'));
+				mail(Yii::app()->params['adminEmail'], $subject, $model->body, $headers);
+				Yii::app()->user->setFlash('contact', Yii::t('app', 'Thank you for contacting us. We will respond to you as soon as possible.'));
 				$this->refresh();
 			}
 		}
-		$this->render('contact',array('model'=>$model));
+		$this->render('contact', array('model' => $model));
 	}
 
 	/**
@@ -88,4 +90,5 @@ class SiteController extends Controller
 		Yii::app()->user->logout();
 		$this->redirect(Yii::app()->homeUrl);
 	}
+
 }
