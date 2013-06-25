b9d5812036a7ebc3fcf1da93d8f06bc0252df633
Fredrik Wollsén <fredrik@neam.se>
2013-06-25 22:03:30 +0300

    Auto-formatting

--- app/views/htmlChunk/_elements.php
+++ app/views/htmlChunk/_elements.php
@@ -1,23 +1,23 @@
 <div class="row">
 	<div class="span8"> <!-- main inputs -->
 
-			<?php
-			$editorOptions = array('rows' => 5, 'options' => array(
-					'locale' => 'en',
-					'link' => true,
-					'image' => false,
-					'color' => false,
-					'html' => true,
-			));
+		<?php
+		$editorOptions = array('rows' => 5, 'options' => array(
+				'locale' => 'en',
+				'link' => true,
+				'image' => false,
+				'color' => false,
+				'html' => true,
+		));
 
-			$this->widget('bootstrap.widgets.TbTabs', array(
-				'tabs' => array(
-					array('label' => 'en', 'content' => $form->html5EditorRow($model, 'markup', $editorOptions), 'active' => true),
-					array('label' => 'de', 'content' => $form->html5EditorRow($model, 'markup_de', $editorOptions)),
-					array('label' => 'foo', 'content' => $form->html5EditorRow($model, 'markup_foo', $editorOptions)),
-				),
-			));
-			?>
+		$this->widget('bootstrap.widgets.TbTabs', array(
+			'tabs' => array(
+				array('label' => 'en', 'content' => $form->html5EditorRow($model, 'markup', $editorOptions), 'active' => true),
+				array('label' => 'de', 'content' => $form->html5EditorRow($model, 'markup_de', $editorOptions)),
+				array('label' => 'foo', 'content' => $form->html5EditorRow($model, 'markup_foo', $editorOptions)),
+			),
+		));
+		?>
 
 
 	</div> <!-- main inputs -->
