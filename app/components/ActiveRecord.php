d05553efae5844e6566eb44ff1732d381a2b2beb
Fredrik Wollsén <fredrik@neam.se>
2013-06-25 08:29:40 +0300

    CTimestampBehavior activated for all models

--- app/components/ActiveRecord.php
+++ app/components/ActiveRecord.php
@@ -3,4 +3,19 @@
 class ActiveRecord extends CActiveRecord
 {
 
+	public function behaviors()
+	{
+
+		$behaviors = array();
+
+		$behaviors['CTimestampBehavior'] = array(
+			'class' => 'zii.behaviors.CTimestampBehavior',
+			'createAttribute' => 'created',
+			'updateAttribute' => 'modified',
+		);
+
+
+		return array_merge(parent::behaviors(), $behaviors);
+	}
+
 }
