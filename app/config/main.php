1d164a531a7d7c33ab13f37ee614a6e81b49cfb1
Fredrik Wollsén <fredrik@neam.se>
2013-06-25 16:15:45 +0300

    Including coreCss in YiiBooster as necessary until we can prevent specifically bootstrapCss to be included

--- app/config/main.php
+++ app/config/main.php
@@ -213,7 +213,7 @@ $mainConfig = array(
         ),
         'bootstrap' => array(
             'class' => 'vendor.clevertech.yiibooster.src.components.Bootstrap',
-            'coreCss' => false, // whether to register the Bootstrap core CSS (bootstrap.min.css), defaults to true
+            'coreCss' => true, // whether to register the Bootstrap core CSS (bootstrap.min.css), defaults to true
             'responsiveCss' => false, // whether to register the Bootstrap responsive CSS (bootstrap-responsive.min.css), default to false
             'plugins' => array(
                 // Optionally you can configure the "global" plugins (button, popover, tooltip and transition)
