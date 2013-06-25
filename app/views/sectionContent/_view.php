748adbb8910ef91fc4863be5d0bcf85ac92d032f
Fredrik Wollsén <fredrik@neam.se>
2013-06-25 16:20:33 +0300

    Added "Section content has no linked content"

--- app/views/sectionContent/_view.php
+++ app/views/sectionContent/_view.php
@@ -2,30 +2,20 @@
 
 	<?php if (!is_null($data->html_chunk_id)): ?>
 		<?php $this->renderPartial('/htmlChunk/_view', array('data' => $data->htmlChunk)); ?>
-	<?php endif; ?>
-
-	<?php if (!is_null($data->viz_view_id)): ?>
+	<?php elseif (!is_null($data->viz_view_id)): ?>
 		<?php $this->renderPartial('/vizView/_view', array('data' => $data->vizView)); ?>
-	<?php endif; ?>
-
-	<?php if (!is_null($data->video_file_id)): ?>
+	<?php elseif (!is_null($data->video_file_id)): ?>
 		<?php $this->renderPartial('/videoFile/_view', array('data' => $data->videoFile)); ?>
-	<?php endif; ?>
-
-	<?php if (!is_null($data->teachers_guide_id)): ?>
+	<?php elseif (!is_null($data->teachers_guide_id)): ?>
 		<?php $this->renderPartial('/teachersGuide/_view', array('data' => $data->teachersGuide)); ?>
-	<?php endif; ?>
-
-	<?php if (!is_null($data->exercise_id)): ?>
+	<?php elseif (!is_null($data->exercise_id)): ?>
 		<?php $this->renderPartial('/exercise/_view', array('data' => $data->exercise)); ?>
-	<?php endif; ?>
-
-	<?php if (!is_null($data->presentation_id)): ?>
+	<?php elseif (!is_null($data->presentation_id)): ?>
 		<?php $this->renderPartial('/presentation/_view', array('data' => $data->presentation)); ?>
-	<?php endif; ?>
-
-	<?php if (!is_null($data->data_chunk_id)): ?>
+	<?php elseif (!is_null($data->data_chunk_id)): ?>
 		<?php $this->renderPartial('/dataChunk/_view', array('data' => $data->dataChunk)); ?>
+	<?php else: ?>
+		<?php echo Yii::t('app', 'Section content has no linked content'); ?>
 	<?php endif; ?>
 
 </div>
