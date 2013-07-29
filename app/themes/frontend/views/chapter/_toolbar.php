1d25ff7581d72cadd44f044b72fa1f65aed50687
Fredrik Wollsén <fredrik@neam.se>
2013-07-29 21:51:27 +0300

    Restored chapter frontend toolbar

--- app/themes/frontend/views/chapter/_toolbar.php
+++ app/themes/frontend/views/chapter/_toolbar.php
@@ -2,11 +2,33 @@
     <div class="btn-group">
         <?php  ?><?php
         switch ($this->action->id) {
+            case "index":
+                $this->widget("bootstrap.widgets.TbButton", array(
+                    "label" => Yii::t("crud", "Manage"),
+                    "icon" => "icon-list-alt",
+                    "url" => array("chapter/admin")
+                ));
+                $this->widget("bootstrap.widgets.TbButton", array(
+                    "label" => Yii::t("crud", "Create"),
+                    "icon" => "icon-plus",
+                    "url" => array("chapter/create")
+                ));
+                break;
             case "view":
+                $this->widget("bootstrap.widgets.TbButton", array(
+                    "label" => Yii::t("crud", "Manage"),
+                    "icon" => "icon-list-alt",
+                    "url" => array("admin")
+                ));
                 $this->widget("bootstrap.widgets.TbButton", array(
                     "label" => Yii::t("crud", "Update"),
                     "icon" => "icon-edit",
                     "url" => array("update", "id" => $model->{$model->tableSchema->primaryKey})
+                ));
+                $this->widget("bootstrap.widgets.TbButton", array(
+                    "label" => Yii::t("crud", "Create"),
+                    "icon" => "icon-plus",
+                    "url" => array("create")
                 ));
                 $this->widget("bootstrap.widgets.TbButton", array(
                         "label" => Yii::t("crud", "Delete"),
@@ -19,5 +41,6 @@
                 );
                 break;
         }
-        ?>    </div>
+        ?>
+    </div>
 </div>
