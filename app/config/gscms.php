d319830a22d997e1a1f55b36d726864e400e400f
Fredrik Wollsén <fredrik@neam.se>
2013-07-29 17:13:38 +0300

    Auto-formatting

--- app/config/gscms.php
+++ app/config/gscms.php
@@ -3,10 +3,9 @@
 // Configuration specific to Gapminder School CMS
 
 $envbootstrap = dirname(__FILE__) . '/../../../common/settings/envbootstrap.php';
-if (!is_readable($envbootstrap))
-{
-	echo "Main envbootstrap file not available.";
-	die(2);
+if (!is_readable($envbootstrap)) {
+    echo "Main envbootstrap file not available.";
+    die(2);
 }
 require_once($envbootstrap);
 
@@ -22,62 +21,62 @@ Yii::setPathOfAlias('frontend', $root . DIRECTORY_SEPARATOR . 'frontend');
 Yii::setPathOfAlias('i18n', $root . DIRECTORY_SEPARATOR . 'i18n');
 
 $gscmsConfig = array(
-	'name' => 'Gapminder School CMS',
-	'language' => 'en', // default language, see also components.langHandler
-	'aliases' => array(
-		// i18n-columns
+    'name' => 'Gapminder School CMS',
+    'language' => 'en', // default language, see also components.langHandler
+    'aliases' => array(
+        // i18n-columns
         'i18n-columns' => 'vendor.neam.yii-i18n-columns',
-	),
-	'import' => array(
-		'i18n-columns.behaviors.I18nColumnsBehavior',
-	),
-	'modules' => array(
-		// uncomment the following to enable the Gii tool
-		'gii' => array(
-			'password' => YII_GII_PASSWORD,
-		),
-	),
-	'components' => array(
-		'urlManager' => array(
-			'urlFormat' => 'path',
-			'showScriptName' => false,
-			'rules' => array(
-				'/' => 'site/index',
-			),
-		),
-		'db' => array(
-			'connectionString' => 'mysql:host=' . YII_DB_HOST . (defined('YII_DB_PORT') ? ';port=' . YII_DB_PORT : '') . ';dbname=' . YII_DB_NAME,
-			'emulatePrepare' => true,
-			'username' => YII_DB_USER,
-			'password' => YII_DB_PASSWORD,
-			'charset' => 'utf8',
-			'enableParamLogging' => true, // Log SQL parameters
-		//'schemaCachingDuration'=>3600*24,
-		),
-		'langHandler' => array(
-			'languages' => array(
-				'en',
-				'es',
-				'fa',
-				'hi',
-				'pt',
-				'sv',
+    ),
+    'import' => array(
+        'i18n-columns.behaviors.I18nColumnsBehavior',
+    ),
+    'modules' => array(
+        // uncomment the following to enable the Gii tool
+        'gii' => array(
+            'password' => YII_GII_PASSWORD,
+        ),
+    ),
+    'components' => array(
+        'urlManager' => array(
+            'urlFormat' => 'path',
+            'showScriptName' => false,
+            'rules' => array(
+                '/' => 'site/index',
+            ),
+        ),
+        'db' => array(
+            'connectionString' => 'mysql:host=' . YII_DB_HOST . (defined('YII_DB_PORT') ? ';port=' . YII_DB_PORT : '') . ';dbname=' . YII_DB_NAME,
+            'emulatePrepare' => true,
+            'username' => YII_DB_USER,
+            'password' => YII_DB_PASSWORD,
+            'charset' => 'utf8',
+            'enableParamLogging' => true, // Log SQL parameters
+            //'schemaCachingDuration'=>3600*24,
+        ),
+        'langHandler' => array(
+            'languages' => array(
+                'en',
+                'es',
+                'fa',
+                'hi',
+                'pt',
+                'sv',
                 'cn',
                 'de',
-			)
-		),
-		'messages' => array(
-			'class' => 'P3PhpMessageSource',
-			'mappings' => array(
-				'en_us' => 'en',
-				'es_es' => 'es',
-				'fa_ir' => 'fa',
-				'hi_in' => 'hi',
-				'pt_pt' => 'pt',
-				'sv_se' => 'sv',
-			)
-		),
-	)
+            )
+        ),
+        'messages' => array(
+            'class' => 'P3PhpMessageSource',
+            'mappings' => array(
+                'en_us' => 'en',
+                'es_es' => 'es',
+                'fa_ir' => 'fa',
+                'hi_in' => 'hi',
+                'pt_pt' => 'pt',
+                'sv_se' => 'sv',
+            )
+        ),
+    )
 );
 
 return $gscmsConfig;
