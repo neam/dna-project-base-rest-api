eb9dae300c377cdcc332e080b328343f82085b7b
Fredrik Wollsén <fredrik@neam.se>
2013-09-06 14:11:23 -0700

    Generated base models

--- app/models/BaseTeachersGuide.php
+++ app/models/BaseTeachersGuide.php
@@ -54,7 +54,7 @@ abstract class BaseTeachersGuide extends ActiveRecord
         return array_merge(
             parent::behaviors(), array(
                 'savedRelated' => array(
-                    'class' => 'gii-template-collection.components.CSaveRelationsBehavior'
+                    'class' => 'vendor.schmunk42.relation.behaviors.GtcSaveRelationsBehavior'
                 )
             )
         );
@@ -105,43 +105,6 @@ abstract class BaseTeachersGuide extends ActiveRecord
         return new CActiveDataProvider(get_class($this), array(
             'criteria' => $criteria,
         ));
-    }
-
-    /**
-     * Returns a model used to populate a filterable, searchable
-     * and sortable CGridView with the records found by a model relation.
-     *
-     * Usage:
-     * $relatedSearchModel = $model->getRelatedSearchModel('relationName');
-     *
-     * Then, when invoking CGridView:
-     *    ...
-     *        'dataProvider' => $relatedSearchModel->search(),
-     *        'filter' => $relatedSearchModel,
-     *    ...
-     * @returns CActiveRecord
-     */
-    public function getRelatedSearchModel($name)
-    {
-
-        $md = $this->getMetaData();
-        if (!isset($md->relations[$name])) {
-            throw new CDbException(Yii::t('yii', '{class} does not have relation "{name}".', array('{class}' => get_class($this), '{name}' => $name)));
-        }
-
-        $relation = $md->relations[$name];
-        if (!($relation instanceof CHasManyRelation)) {
-            throw new CException("Currently works with HAS_MANY relations only");
-        }
-
-        $className = $relation->className;
-        $related = new $className('search');
-        $related->unsetAttributes();
-        $related->{$relation->foreignKey} = $this->primaryKey;
-        if (isset($_GET[$className])) {
-            $related->attributes = $_GET[$className];
-        }
-        return $related;
     }
 
 }
