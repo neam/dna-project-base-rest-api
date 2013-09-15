99b3633e6661798078f11dc82d6f8cb6a51f275e
Fredrik Wollsén <fredrik@neam.se>
2013-09-14 23:38:19 -0700

    Added yii sql migration generator

--- app/config/gscms.php
+++ app/config/gscms.php
@@ -53,6 +53,7 @@ $gscmsConfig = array(
             'ipFilters' => array('127.0.0.1', '::1'),
             'generatorPaths' => array(
                 'vendor.phundament.gii-template-collection', // giix generators
+                'vendor.mihanentalpo.yii-sql-migration-generator',
                 'bootstrap.gii', // bootstrap generator
             ),
         ),
