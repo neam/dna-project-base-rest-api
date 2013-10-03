15516e14b02b5745f542645753021dc03930d77c
Fredrik Wollsén <fredrik@neam.se>
2013-10-03 21:14:52 +0200

    Updated item workflow

--- app/workflows/ItemWorkflow.php
+++ app/workflows/ItemWorkflow.php
@@ -25,12 +25,16 @@ class ItemWorkflow
         // Do not store custom classes if not in memory only
         $nodeInputClass = $inMemory ? "gscmsLabeledWorkflowNodeInput" : "ezcWorkflowNodeInput";
 
+        $this->process_steps['new'] = new $nodeInputClass(array(
+            'Label: New' => new ezcWorkflowConditionIsAnything(),
+            'draft' => new ezcWorkflowConditionIsTrue(),
+        ));
         $this->process_steps['draft'] = new $nodeInputClass(array(
-            'draft' => new ezcWorkflowConditionIsAnything(),
+            'Label: Draft' => new ezcWorkflowConditionIsAnything(),
             'previewable' => new ezcWorkflowConditionIsTrue(),
         ));
         $this->process_steps['preview'] = new $nodeInputClass(array(
-            'preview' => new ezcWorkflowConditionIsAnything(),
+            'Label: Preview' => new ezcWorkflowConditionIsAnything(),
             'publishable' => new ezcWorkflowConditionIsTrue(),
             'candidate' => new ezcWorkflowConditionIsTrue(),
             'approved' => new ezcWorkflowConditionIsTrue(),
@@ -38,19 +42,20 @@ class ItemWorkflow
             'translated' => new ezcWorkflowConditionIsTrue(),
         ));
         $this->process_steps['public'] = new $nodeInputClass(array(
-            'public' => new ezcWorkflowConditionIsAnything(),
+            'Label: Public' => new ezcWorkflowConditionIsAnything(),
             'replaced' => new ezcWorkflowConditionIsTrue(),
         ));
         $this->process_steps['replaced'] = new $nodeInputClass(array(
-            'replaced' => new ezcWorkflowConditionIsAnything(),
+            'Label: Replaced' => new ezcWorkflowConditionIsAnything(),
             'stayhere' => new ezcWorkflowConditionIsFalse(),
         ));
     }
 
     function connectProcessSteps()
     {
-        $this->workflow->startNode->addOutNode($this->process_steps['draft']);
+        $this->workflow->startNode->addOutNode($this->process_steps['new']);
 
+        $this->process_steps['new']->addOutNode($this->process_steps['draft']);
         $this->process_steps['draft']->addOutNode($this->process_steps['preview']);
         $this->process_steps['preview']->addOutNode($this->process_steps['public']);
         $this->process_steps['public']->addOutNode($this->process_steps['replaced']);
