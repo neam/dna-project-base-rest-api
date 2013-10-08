32abdf9ce9fa7fa69de68fcab3ab5bc5f967b550
Fredrik Wollsén <fredrik@neam.se>
2013-10-08 02:02:22 +0200

    Set continueAuthoring actions to use backend2 theme

--- app/config/main.php
+++ app/config/main.php
@@ -291,6 +291,7 @@ $mainConfig = array(
                 '^([^/]*)/update' => 'backend2',
                 '^([^/]*)/admin' => 'backend2',
                 '^([^/]*)/create' => 'backend2',
+                '^([^/]*)/continueAuthoring' => 'backend2',
                 '^([^/]*)/translate(.*)' => 'backend2',
                 '^([^/]*)/author(.*)' => 'backend2',
                 '^site/giiscript' => 'backend2',
