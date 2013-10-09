0f30fa5d9103d0e905c5a4cfb1db1db3b128f685
Fredrik Wollsén <fredrik@neam.se>
2013-10-09 15:57:07 +0300

    Reverted viz view _view view (!) to working state again

--- app/themes/frontend/views/vizView/_view.php
+++ app/themes/frontend/views/vizView/_view.php
@@ -1,71 +1,10 @@
 <div class="view">
 
-    <b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
-    <?php echo CHtml::link(CHtml::encode($data->id), array('vizView/view', 'id' => $data->id)); ?>
-    <br/>
+    <?php echo str_replace("{language}", Yii::app()->language, $data->embed_template); ?>
 
-    <b><?php echo CHtml::encode($data->getAttributeLabel('version')); ?>:</b>
-    <?php echo CHtml::encode($data->version); ?>
-    <br/>
-
-    <b><?php echo CHtml::encode($data->getAttributeLabel('cloned_from_id')); ?>:</b>
-    <?php echo CHtml::encode($data->cloned_from_id); ?>
-    <br/>
-
-    <b><?php echo CHtml::encode($data->getAttributeLabel('title_en')); ?>:</b>
-    <?php echo CHtml::encode($data->title_en); ?>
-    <br/>
-
-    <b><?php echo CHtml::encode($data->getAttributeLabel('embed_template')); ?>:</b>
-    <?php echo CHtml::encode($data->embed_template); ?>
-    <br/>
-
-    <b><?php echo CHtml::encode($data->getAttributeLabel('authoring_workflow_execution_id')); ?>:</b>
-    <?php echo CHtml::encode($data->authoring_workflow_execution_id); ?>
-    <br/>
-
-    <b><?php echo CHtml::encode($data->getAttributeLabel('created')); ?>:</b>
-    <?php echo CHtml::encode($data->created); ?>
-    <br/>
-
-    <?php /*
-    <b><?php echo CHtml::encode($data->getAttributeLabel('modified')); ?>:</b>
-    <?php echo CHtml::encode($data->modified); ?>
-    <br />
-
-    <b><?php echo CHtml::encode($data->getAttributeLabel('title_es')); ?>:</b>
-    <?php echo CHtml::encode($data->title_es); ?>
-    <br />
-
-    <b><?php echo CHtml::encode($data->getAttributeLabel('title_fa')); ?>:</b>
-    <?php echo CHtml::encode($data->title_fa); ?>
-    <br />
-
-    <b><?php echo CHtml::encode($data->getAttributeLabel('title_hi')); ?>:</b>
-    <?php echo CHtml::encode($data->title_hi); ?>
-    <br />
-
-    <b><?php echo CHtml::encode($data->getAttributeLabel('title_pt')); ?>:</b>
-    <?php echo CHtml::encode($data->title_pt); ?>
-    <br />
-
-    <b><?php echo CHtml::encode($data->getAttributeLabel('title_sv')); ?>:</b>
-    <?php echo CHtml::encode($data->title_sv); ?>
-    <br />
-
-    <b><?php echo CHtml::encode($data->getAttributeLabel('title_cn')); ?>:</b>
-    <?php echo CHtml::encode($data->title_cn); ?>
-    <br />
-
-    <b><?php echo CHtml::encode($data->getAttributeLabel('title_de')); ?>:</b>
-    <?php echo CHtml::encode($data->title_de); ?>
-    <br />
-
-    */
-    ?>
     <?php if (Yii::app()->user->checkAccess('VizView.*')): ?>
         <div class="admin-container show">
-            <?php echo CHtml::link('<i class="icon-edit"></i> ' . Yii::t('model', 'Update {model}', array('{model}' => Yii::t('model', 'Viz View'))), array('vizView/update', 'id' => $data->id, 'returnUrl' => Yii::app()->request->url), array('class' => 'btn')); ?>
+            <?php echo CHtml::link('<i class="icon-edit"></i> ' . Yii::t('crud', 'Update {model}', array('{model}' => Yii::t('crud', 'Viz View'))), array('vizView/update', 'id' => $data->id, 'returnUrl' => Yii::app()->request->url), array('class' => 'btn')); ?>
         </div>
     <?php endif; ?>
 
