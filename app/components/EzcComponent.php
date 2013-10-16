1c1029ba0e56d547ebcccc4c8d4c1cfd628ba27e
Fredrik Wollsén <fredrik@neam.se>
2013-10-16 12:15:17 +0300

    Disabled autoloader for ezc (achieved through composer autoloader instead)

--- app/components/EzcComponent.php
+++ app/components/EzcComponent.php
@@ -5,13 +5,6 @@ class EzcComponent extends CApplicationComponent
     public $ezcAlias = 'vendor.zetacomponents';
     private $_db = null;
 
-    public function init()
-    {
-        // Load ez components auto-loader
-        require_once Yii::getPathOfAlias($this->ezcAlias) . '/Base/src/base.php';
-        Yii::registerAutoloader(array('ezcBase', 'autoload'), true);
-    }
-
     public function & getDb()
     {
         if (is_null($this->_db)) {
