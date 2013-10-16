737f76b126673a4d82d267ade012351b7ec19bab
Fredrik Wollsén <fredrik@neam.se>
2013-10-16 15:22:50 +0300

    Proper item label for snapshot

--- app/models/Snapshot.php
+++ app/models/Snapshot.php
@@ -20,7 +20,7 @@ class Snapshot extends BaseSnapshot
 
     public function getItemLabel()
     {
-        return parent::getItemLabel();
+        return (string) $this->title;
     }
 
     public function behaviors()
