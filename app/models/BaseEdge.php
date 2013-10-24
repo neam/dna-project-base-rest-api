55e2b57622a6660bd6c0678f39bcabcfe89fff40
Fredrik Wollsén <fredrik@neam.se>
2013-10-24 15:06:46 +0200

    Generated edges model with new fields

--- app/models/BaseEdge.php
+++ app/models/BaseEdge.php
@@ -7,6 +7,10 @@
  * @property string $id
  * @property string $from_node_id
  * @property string $to_node_id
+ * @property integer $weight
+ * @property string $title
+ * @property string $created
+ * @property string $modified
  *
  * Relations of table "edge" available as properties of the model:
  * @property Node $fromNode
@@ -30,8 +34,12 @@ abstract class BaseEdge extends ActiveRecord
         return array_merge(
             parent::rules(), array(
                 array('from_node_id, to_node_id', 'required'),
+                array('weight, title, created, modified', 'default', 'setOnEmpty' => true, 'value' => null),
+                array('weight', 'numerical', 'integerOnly' => true),
                 array('from_node_id, to_node_id', 'length', 'max' => 20),
-                array('id, from_node_id, to_node_id', 'safe', 'on' => 'search'),
+                array('title', 'length', 'max' => 255),
+                array('created, modified', 'safe'),
+                array('id, from_node_id, to_node_id, weight, title, created, modified', 'safe', 'on' => 'search'),
             )
         );
     }
@@ -54,9 +62,11 @@ abstract class BaseEdge extends ActiveRecord
 
     public function relations()
     {
-        return array(
-            'fromNode' => array(self::BELONGS_TO, 'Node', 'from_node_id'),
-            'toNode' => array(self::BELONGS_TO, 'Node', 'to_node_id'),
+        return array_merge(
+            parent::relations(), array(
+                'fromNode' => array(self::BELONGS_TO, 'Node', 'from_node_id'),
+                'toNode' => array(self::BELONGS_TO, 'Node', 'to_node_id'),
+            )
         );
     }
 
@@ -66,6 +76,10 @@ abstract class BaseEdge extends ActiveRecord
             'id' => Yii::t('model', 'ID'),
             'from_node_id' => Yii::t('model', 'From Node'),
             'to_node_id' => Yii::t('model', 'To Node'),
+            'weight' => Yii::t('model', 'Weight'),
+            'title' => Yii::t('model', 'Title'),
+            'created' => Yii::t('model', 'Created'),
+            'modified' => Yii::t('model', 'Modified'),
         );
     }
 
@@ -78,6 +92,10 @@ abstract class BaseEdge extends ActiveRecord
         $criteria->compare('t.id', $this->id, true);
         $criteria->compare('t.from_node_id', $this->from_node_id);
         $criteria->compare('t.to_node_id', $this->to_node_id);
+        $criteria->compare('t.weight', $this->weight);
+        $criteria->compare('t.title', $this->title, true);
+        $criteria->compare('t.created', $this->created, true);
+        $criteria->compare('t.modified', $this->modified, true);
 
 
         return $criteria;
