e5a716d0dbe33989dfef228e563a5cde1d8cd7c7
Fredrik Wollsén <fredrik@neam.se>
2013-10-28 17:51:26 +0100

    Node _view renders the correct sub-view

--- app/themes/frontend/views/node/_view.php
+++ app/themes/frontend/views/node/_view.php
@@ -1,21 +1,2 @@
-<div class="view">
-
-    <b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
-    <?php echo CHtml::link(CHtml::encode($data->id), array('node/view', 'id' => $data->id)); ?>
-    <br/>
-
-    <b><?php echo CHtml::encode($data->getAttributeLabel('created')); ?>:</b>
-    <?php echo CHtml::encode($data->created); ?>
-    <br/>
-
-    <b><?php echo CHtml::encode($data->getAttributeLabel('modified')); ?>:</b>
-    <?php echo CHtml::encode($data->modified); ?>
-    <br/>
-
-    <?php if (Yii::app()->user->checkAccess('Node.*')): ?>
-        <div class="admin-container show">
-            <?php echo CHtml::link('<i class="icon-edit"></i> ' . Yii::t('model', 'Update {model}', array('{model}' => Yii::t('model', 'Node'))), array('node/update', 'id' => $data->id, 'returnUrl' => Yii::app()->request->url), array('class' => 'btn')); ?>
-        </div>
-    <?php endif; ?>
-
-</div>
+<?php
+$this->renderPartial('/' . lcfirst(get_class($data)) . '/_view', compact('data'));
