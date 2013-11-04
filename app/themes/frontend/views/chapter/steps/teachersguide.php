9f194fc864fe9e52a300bdb2b5961699af50495b
Fredrik Wollsén <fredrik@neam.se>
2013-11-04 17:37:56 +0200

    Switched to ckeditor on teachersguide for evaluation

--- app/themes/frontend/views/chapter/steps/teachersguide.php
+++ app/themes/frontend/views/chapter/steps/teachersguide.php
@@ -1,8 +1,12 @@
-<?php echo $form->html5EditorRow($model, 'teachers_guide', array('rows' => 6, 'cols' => 50, 'class' => 'span6', 'options' => array(
+<?php if (false) echo $form->html5EditorRow($model, 'teachers_guide', array('rows' => 6, 'cols' => 50, 'class' => 'span6', 'options' => array(
     'link' => true,
     'image' => false,
     'color' => false,
     'html' => true,
+))); ?>
+
+<?php echo $form->ckEditorRow($model, 'teachers_guide', array('rows' => 6, 'cols' => 50, 'class' => 'span6', 'options' => array(
+    'fullpage' => 'js:true', 'width' => '640', 'resize_maxWidth' => '640', 'resize_minWidth' => '320'
 ))); ?>
 
 <p class="alert alert-info help-block">
