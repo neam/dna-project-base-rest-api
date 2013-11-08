37246e7dbea95f7521970801a7dbdf215aafccdf
Johan Ström <johan@johanstrom.se>
2013-11-08 12:50:30 +0100

    Upload pofile now works properly

--- app/themes/frontend/views/poFile/steps/file.php
+++ app/themes/frontend/views/poFile/steps/file.php
@@ -1,11 +1,48 @@
 <?php
+
+Yii::app()->bootstrap->registerAssetCss('../select2/select2.css');
+Yii::app()->bootstrap->registerAssetJs('../select2/select2.js');
+
+$baseUrl = Yii::app()->request->baseUrl;
+
+$noneLabel = Yii::t('app', 'None');
+$chooseBelowLabel = Yii::t('app', 'Click to choose existing or upload new below');
+
+$select2js = <<<EOF
+
+function format(state) {
+    if (!state.id && !state.text) return "<div class='select2-text'>{$noneLabel} - {$chooseBelowLabel}</div>";
+    if (!state.id) return state.text;
+    return "<div><img class='select2-thumb' src='{$baseUrl}/p3media/file/image?preset=select2-thumb&id=" + state.id.toLowerCase() + "'/></div><div class='select2-text'>" + state.text + "</div>";
+}
+
+var select2opts = {
+    formatResult: format,
+    formatSelection: format,
+    //escapeMarkup: function(m) { return m; }
+};
+
+//$("#PoFile_original_media_id").data('select2opts', select2opts);
+//$("#PoFile_original_media_id").select2($("#PoFile_original_media_id").data('select2opts'));
+
+EOF;
+
+Yii::app()->clientScript->registerScript('step_file-select2', $select2js);
+
+$criteria = new CDbCriteria();
+$criteria->addCondition("mimetype IN ('text/x-po','text/plain')");
+$criteria->addCondition("t.type = 1");
+$criteria->limit = 100;
+$criteria->order = "createdAt DESC";
+
 $input = $this->widget(
     '\GtcRelation',
     array(
         'model' => $model,
         'relation' => 'originalMedia',
         'fields' => 'itemLabel',
-        'allowEmpty' => true,
+        'criteria' => $criteria,
+        'allowEmpty' => $noneLabel,
         'style' => 'dropdownlist',
         'htmlOptions' => array(
             'checkAll' => 'all'
@@ -19,7 +56,34 @@ echo $form->customRow($model, 'original_media_id', $input);
 $formId = 'pofile-original_media_id-' . \uniqid() . '-form';
 ?>
 
-<?php if ($model->getAttributeHint("thumbnail")): ?>
+<div class="control-group">
+    <div class="controls">
+        <?php
+        echo $this->widget('bootstrap.widgets.TbButton', array(
+            'label' => Yii::t('app', 'Upload'),
+            'icon' => 'icon-plus',
+            'htmlOptions' => array(
+                'data-toggle' => 'modal',
+                'data-target' => '#' . $formId . '-modal',
+            ),
+        ), true);
+        ?>
+    </div>
+</div>
+
+<?php
+$this->beginClip('modal:' . $formId . '-modal');
+$this->renderPartial('//p3Media/_modal_form', array(
+    'formId' => $formId,
+    'inputSelector' => '#PoFile_original_media_id',
+    'model' => new P3Media,
+    'pk' => 'id',
+    'field' => 'itemLabel',
+));
+$this->endClip();
+?>
+
+<?php if ($model->getAttributeHint("file")): ?>
     <p class="alert alert-info help-block">
         <?php echo $model->getAttributeHint("file"); ?>
     </p>
