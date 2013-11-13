34fed44abb0fff94a01b448499f2f00bce0aa028
Fredrik Wollsén <fredrik@neam.se>
2013-11-13 17:37:04 +0200

    Validation rule fix

--- app/models/Chapter.php
+++ app/models/Chapter.php
@@ -65,7 +65,7 @@ class Chapter extends BaseChapter
 
                 // Define step-dependent fields - Part 2 - what fields are required at each step?
                 array('title_' . $this->source_language . '', 'required', 'on' => 'draft-step_info,preview-step_info,public-step_info,step_info'),
-                array('slug_' . $this->source_language . ', about_' . $this->source_language . ', thumbnail_media_id', 'required', 'on' => 'step_info'),
+                array('slug_' . $this->source_language . ', about_' . $this->source_language . ', thumbnail_media_id', 'required', 'on' => 'public-step_info,step_info'),
                 array('teachers_guide', 'required', 'on' => 'public-step_teachers_guide,step_teachers_guide'),
                 array('exercises', 'required', 'on' => 'public-step_exercises,step_exercises'),
                 array('videos', 'required', 'on' => 'public-step_videos,step_videos'),
