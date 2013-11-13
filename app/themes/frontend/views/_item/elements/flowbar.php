a8db22afad30b9e78eafff9dccf15dcb64c9aa47
Fredrik Wollsén <fredrik@neam.se>
2013-11-13 17:02:11 +0200

    Showing buttons for all workflows that are not yet done

--- app/themes/frontend/views/_item/elements/flowbar.php
+++ app/themes/frontend/views/_item/elements/flowbar.php
@@ -58,14 +58,16 @@ $actions = $this->itemActions($model);
                             "icon" => "icon-pencil" . ($this->action->id == "draft" ? " icon-white" : null),
                             "url" => array("draft", "id" => $model->{$model->tableSchema->primaryKey}, "step" => $this->nextFlowStep("draft-", $model))
                         ));
-                    } elseif (!$model->qaState()->previewing_welcome) {
+                    }
+                    if (!$model->qaState()->previewing_welcome) {
                         $this->widget("bootstrap.widgets.TbButton", array(
                             "label" => Yii::t("crud", "Prepare for testing"),
                             "type" => $this->action->id == "prepPreshow" ? "inverse" : null,
                             "icon" => "icon-edit" . ($this->action->id == "" ? " icon-white" : null),
                             "url" => array("prepPreshow", "id" => $model->{$model->tableSchema->primaryKey}, "step" => $this->nextFlowStep("preview-", $model))
                         ));
-                    } elseif (!$model->qaState()->candidate_for_public_status) {
+                    }
+                    if (!$model->qaState()->candidate_for_public_status) {
                         $this->widget("bootstrap.widgets.TbButton", array(
                             "label" => Yii::t("crud", "Prepare for publishing"),
                             "type" => $this->action->id == "prepPublish" ? "inverse" : null,
