e934d18d134d955eae81f876b329e12c0be368e6
Fredrik Wollsén <fredrik@neam.se>
2013-11-18 00:17:32 +0100

    Added a unit test that demonstrates trait inheritance

---
+++ tests/codeception/unit/ActionTraitsTest.php
@@ -0,0 +1,74 @@
+<?php
+use Codeception\Util\Stub;
+
+trait IndexActionTrait
+{
+    public function actionIndex()
+    {
+        return "foo";
+    }
+}
+
+class Foo
+{
+    use IndexActionTrait {
+        IndexActionTrait::actionIndex as actionIndexFromTrait;
+    }
+
+    public function actionIndex()
+    {
+        return "bar";
+    }
+}
+
+require('vendor/yiisoft/yii/framework/base/CComponent.php');
+require('vendor/yiisoft/yii/framework/web/CBaseController.php');
+require('vendor/yiisoft/yii/framework/web/CController.php');
+class FooController extends CController
+{
+    use IndexActionTrait {
+        IndexActionTrait::actionIndex as actionIndexFromTrait;
+    }
+
+    public function actionIndex()
+    {
+        return "bar";
+    }
+}
+
+class ActionTraitsTest extends \Codeception\TestCase\Test
+{
+    /**
+     * @var \CodeGuy
+     */
+    protected $codeGuy;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    /**
+     * @test
+     */
+    public function demonstrateTraitInheritance()
+    {
+
+        $controller = new Foo();
+        $this->assertEquals(true, method_exists($controller, 'actionIndex'));
+        $this->assertEquals(true, method_exists($controller, 'actionIndexFromTrait'));
+        $this->assertEquals("bar", $controller->actionIndex());
+        $this->assertEquals("foo", $controller->actionIndexFromTrait());
+
+        $controller = new FooController('Foo');
+        $this->assertEquals(true, method_exists($controller, 'actionIndex'));
+        $this->assertEquals(true, method_exists($controller, 'actionIndexFromTrait'));
+        $this->assertEquals("bar", $controller->actionIndex());
+        $this->assertEquals("foo", $controller->actionIndexFromTrait());
+
+    }
+
+}
