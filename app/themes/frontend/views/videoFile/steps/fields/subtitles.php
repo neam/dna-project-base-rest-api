cd39d6f47fb115930814e245a9f6fa1ae1a1397d
Fredrik Wollsén <fredrik@neam.se>
2013-11-20 11:51:59 +0100

    Increased subtitles editing area

--- app/themes/frontend/views/videoFile/steps/fields/subtitles.php
+++ app/themes/frontend/views/videoFile/steps/fields/subtitles.php
@@ -1,4 +1,4 @@
-<?php echo $form->textAreaRow($model, 'subtitles_' . $model->source_language, array('rows' => 6, 'cols' => 50, 'class' => 'span8')); ?>
+<?php echo $form->textAreaRow($model, 'subtitles_' . $model->source_language, array('rows' => 50, 'cols' => 50, 'class' => 'span8')); ?>
 
 <?php if ($model->getAttributeHint("subtitles")): ?>
     <p class="alert alert-info help-block">
