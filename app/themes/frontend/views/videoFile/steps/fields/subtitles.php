4037fa5a8dc4f772bfa462c53554577a15b25b8e
Johan Ström <johan@johanstrom.se>
2013-11-20 10:47:51 +0100

    Wrap around video subtitles field

--- app/themes/frontend/views/videoFile/steps/fields/subtitles.php
+++ app/themes/frontend/views/videoFile/steps/fields/subtitles.php
@@ -1,38 +1,10 @@
-<?php echo $form->textAreaRow($model, 'subtitles_'.$model->source_language, array('rows' => 6, 'cols' => 50, 'class' => 'span8')); ?>
-<?php if ($this->workflowData["translateInto"]) echo $form->textAreaRow($model, 'subtitles_'.$this->workflowData["translateInto"], array('rows' => 6, 'cols' => 50, 'class' => 'span8')); ?>
+<div class="control-group">
+    <?php echo $form->textAreaRow($model, 'subtitles_' . $model->source_language, array('rows' => 6, 'cols' => 50, 'class' => 'span8')); ?>
+    <?php if ($this->workflowData["translateInto"]) echo $form->textAreaRow($model, 'subtitles_' . $this->workflowData["translateInto"], array('rows' => 6, 'cols' => 50, 'class' => 'span8')); ?>
 
-<?php if ($model->getAttributeHint("subtitles")): ?>
-    <p class="alert alert-info help-block">
-        <?php echo $model->getAttributeHint("subtitles"); ?>
-    </p>
-<?php endif; ?>
-
-
-<?php
-/*
-$input = $this->widget(
-    '\GtcRelation',
-    array(
-        'model' => $model,
-        'relation' => 'originalMedia',
-        'fields' => 'itemLabel',
-        'allowEmpty' => true,
-        'style' => 'dropdownlist',
-        'htmlOptions' => array(
-            'checkAll' => 'all'
-        ),
-    )
-    , true);
-echo $form->customRow($model, 'original_media_id', $input);
-?>
-
-<?php
-$formId = 'videofile-original_media_id-' . \uniqid() . '-form';
-?>
-
-<?php if ($model->getAttributeHint("clip")): ?>
-    <p class="alert alert-info help-block">
-        <?php echo $model->getAttributeHint("clip"); ?>
-    </p>
-<?php endif;
-*/ ?>
+    <?php if ($model->getAttributeHint("subtitles")): ?>
+        <p class="alert alert-info help-block">
+            <?php echo $model->getAttributeHint("subtitles"); ?>
+        </p>
+    <?php endif; ?>
+</div>
