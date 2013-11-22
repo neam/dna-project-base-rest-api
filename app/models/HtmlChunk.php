b33bb04e84d30647435729c9c6c0bb23cb17d0b0
Fredrik Wollsén <fredrik@neam.se>
2013-11-22 14:15:50 +0200

    Item trait usage and item validation validation rules for download link and html chunk

--- app/models/HtmlChunk.php
+++ app/models/HtmlChunk.php
@@ -7,6 +7,8 @@ Yii::import('HtmlChunk.*');
 class HtmlChunk extends BaseHtmlChunk
 {
 
+    use ItemTrait;
+
     // Add your model-specific methods here. This file will not be overriden by gtc except you force it.
     public static function model($className = __CLASS__)
     {
@@ -33,12 +35,51 @@ class HtmlChunk extends BaseHtmlChunk
 
     public function rules()
     {
-        return array_merge(
-            parent::rules()
-        /* , array(
-          array('column1, column2', 'rule1'),
-          array('column3', 'rule2'),
-          ) */
+        $return = array_merge(
+            parent::rules(),
+            $this->statusRequirementsRules(),
+            $this->flowStepRules(),
+            $this->i18nRules(),
+            array(// Ordinary validation rules
+
+            )
+        );
+        Yii::log("model->rules(): " . print_r($return, true), "trace", __METHOD__);
+        return $return;
+    }
+
+    /**
+     * Define status-dependent fields
+     * @return array
+     */
+    public function statusRequirements()
+    {
+        return array(
+            'draft' => array(
+                'markup_' . $this->source_language,
+            ),
+            'preview' => array(),
+            'public' => array(),
+        );
+    }
+
+    /**
+     * Define step-dependent fields
+     * @return array
+     */
+    public function flowSteps()
+    {
+        return array(
+            'markup' => array(
+                'markup_' . $this->source_language,
+            ),
+        );
+    }
+
+    public function flowStepCaptions()
+    {
+        return array(
+            'markup' => Yii::t('app', 'HTML'),
         );
     }
 
