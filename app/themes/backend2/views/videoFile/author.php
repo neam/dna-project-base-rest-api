d2eca79bf6f1b2e598ae4cf7bfd44d178995dfa7
Fredrik Wollsén <fredrik@neam.se>
2013-11-22 11:52:12 +0200

    Removed some unused concept views

--- app/themes/backend2/views/videoFile/author.php
+++
@@ -1,1 +0,0 @@
-todo author ui
--- app/themes/backend2/views/videoFile/translate/title_and_about.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by JetBrains PhpStorm.
- * User: motin
- * Date: 2013-09-14
- * Time: 18:29
- * To change this template use File | Settings | File Templates.
- */
