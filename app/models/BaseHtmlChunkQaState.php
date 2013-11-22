532ff566a6ba794e63c08c9a20ddae109a3006b8
Fredrik Wollsén <fredrik@neam.se>
2013-11-22 14:26:30 +0200

    Added models for html chunk and download link qa state

---
+++ app/models/BaseHtmlChunkQaState.php
@@ -0,0 +1,112 @@
+<?php
+
+/**
+ * This is the model base class for the table "html_chunk_qa_state".
+ *
+ * Columns in table "html_chunk_qa_state" available as properties of the model:
+ * @property string $id
+ * @property string $status
+ * @property integer $draft_validation_progress
+ * @property integer $preview_validation_progress
+ * @property integer $public_validation_progress
+ * @property integer $approval_progress
+ * @property integer $proofing_progress
+ * @property integer $previewing_welcome
+ * @property integer $candidate_for_public_status
+ * @property integer $markup_approved
+ * @property integer $markup_proofed
+ *
+ * Relations of table "html_chunk_qa_state" available as properties of the model:
+ * @property HtmlChunk[] $htmlChunks
+ */
+abstract class BaseHtmlChunkQaState extends ActiveRecord
+{
+
+    public static function model($className = __CLASS__)
+    {
+        return parent::model($className);
+    }
+
+    public function tableName()
+    {
+        return 'html_chunk_qa_state';
+    }
+
+    public function rules()
+    {
+        return array_merge(
+            parent::rules(), array(
+                array('status, draft_validation_progress, preview_validation_progress, public_validation_progress, approval_progress, proofing_progress, previewing_welcome, candidate_for_public_status, markup_approved, markup_proofed', 'default', 'setOnEmpty' => true, 'value' => null),
+                array('draft_validation_progress, preview_validation_progress, public_validation_progress, approval_progress, proofing_progress, previewing_welcome, candidate_for_public_status, markup_approved, markup_proofed', 'numerical', 'integerOnly' => true),
+                array('status', 'length', 'max' => 255),
+                array('id, status, draft_validation_progress, preview_validation_progress, public_validation_progress, approval_progress, proofing_progress, previewing_welcome, candidate_for_public_status, markup_approved, markup_proofed', 'safe', 'on' => 'search'),
+            )
+        );
+    }
+
+    public function getItemLabel()
+    {
+        return (string) $this->status;
+    }
+
+    public function behaviors()
+    {
+        return array_merge(
+            parent::behaviors(), array(
+                'savedRelated' => array(
+                    'class' => '\GtcSaveRelationsBehavior'
+                )
+            )
+        );
+    }
+
+    public function relations()
+    {
+        return array_merge(
+            parent::relations(), array(
+                'htmlChunks' => array(self::HAS_MANY, 'HtmlChunk', 'html_chunk_qa_state_id'),
+            )
+        );
+    }
+
+    public function attributeLabels()
+    {
+        return array(
+            'id' => Yii::t('model', 'ID'),
+            'status' => Yii::t('model', 'Status'),
+            'draft_validation_progress' => Yii::t('model', 'Draft Validation Progress'),
+            'preview_validation_progress' => Yii::t('model', 'Preview Validation Progress'),
+            'public_validation_progress' => Yii::t('model', 'Public Validation Progress'),
+            'approval_progress' => Yii::t('model', 'Approval Progress'),
+            'proofing_progress' => Yii::t('model', 'Proofing Progress'),
+            'previewing_welcome' => Yii::t('model', 'Previewing Welcome'),
+            'candidate_for_public_status' => Yii::t('model', 'Candidate For Public Status'),
+            'markup_approved' => Yii::t('model', 'Markup Approved'),
+            'markup_proofed' => Yii::t('model', 'Markup Proofed'),
+        );
+    }
+
+    public function searchCriteria($criteria = null)
+    {
+        if (is_null($criteria)) {
+            $criteria = new CDbCriteria;
+        }
+
+        $criteria->compare('t.id', $this->id, true);
+        $criteria->compare('t.status', $this->status, true);
+        $criteria->compare('t.draft_validation_progress', $this->draft_validation_progress);
+        $criteria->compare('t.preview_validation_progress', $this->preview_validation_progress);
+        $criteria->compare('t.public_validation_progress', $this->public_validation_progress);
+        $criteria->compare('t.approval_progress', $this->approval_progress);
+        $criteria->compare('t.proofing_progress', $this->proofing_progress);
+        $criteria->compare('t.previewing_welcome', $this->previewing_welcome);
+        $criteria->compare('t.candidate_for_public_status', $this->candidate_for_public_status);
+        $criteria->compare('t.markup_approved', $this->markup_approved);
+        $criteria->compare('t.markup_proofed', $this->markup_proofed);
+
+
+        return $criteria;
+
+    }
+
+}
