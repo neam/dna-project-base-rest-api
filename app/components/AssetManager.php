b1b960857b17c2f0d7fed6f47bc5ee45be74cc12
Fredrik Wollsén <fredrik@neam.se>
2013-11-27 16:05:36 +0200

    Restoring cached hostInfo so that CDN_HOST does not affect it for the remaining part of the execution

--- app/components/AssetManager.php
+++ app/components/AssetManager.php
@@ -6,9 +6,11 @@ class AssetManager extends CAssetManager
     {
         if (defined('CDN_HOST')) {
             $_ = $_SERVER['HTTP_HOST'];
+            $_hostInfo = Yii::app()->request->getHostInfo();
             $_SERVER['HTTP_HOST'] = CDN_HOST;
             $baseUrl = Yii::app()->request->getBaseUrl(true) . parent::getBaseUrl();
             $_SERVER['HTTP_HOST'] = $_;
+            Yii::app()->request->setHostInfo($_hostInfo);
             return $baseUrl;
         }
         return parent::getBaseUrl();
