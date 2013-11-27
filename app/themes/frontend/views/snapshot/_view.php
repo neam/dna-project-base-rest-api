06e5ef1a00d72903884ed35dbafe0c2f65f75560
Thiago Costa Porto <thiago@gapminder.org>
2013-11-27 09:41:01 +0100

    Snapshots shares related items through view

--- app/themes/frontend/views/snapshot/_view.php
+++ app/themes/frontend/views/snapshot/_view.php
@@ -1,4 +1,4 @@
-<div class="view">
+<div class="view" style="overflow: hidden;">
 
     <?php if (!is_null($data->embed_override)): ?>
 
@@ -16,9 +16,42 @@
         $markup = $data->tool->embed_template;
         ?>
 
-        <?php $_ = str_replace("{language}", Yii::app()->language, $markup); ?>
-        <?php echo str_replace("{vizabi_state}", $vizabi_state, $_); ?>
+        <?php
+        $relArray = array();
+        $relatedNodes = $data->outNodes;
 
+        if (is_array($relatedNodes)) {
+            foreach ($relatedNodes as $i => $relatedNode) {
+                $item = $relatedNode->item();
+                if (isset($item->thumbnail_media_id)) {
+                    $url = Yii::app()->request->baseUrl;
+
+                    $thumb_url = $url . "/p3media/file/image?preset=related-thumb&id=". $item->thumbnail_media_id;
+
+                    $edge_title = Yii::app()->db->createCommand()
+                        ->select("title")
+                        ->from("edge")
+                        ->where("from_node_id=$data->node_id AND to_node_id=$item->node_id")
+                        ->queryRow();
+
+                    $relArray[] = [
+                        "id" => $item->id,
+                        "thumb_url" => $thumb_url,
+                        "type" => get_class($item),
+                        "title" => $edge_title['title']
+                    ];
+                }
+            }
+        }
+
+        $relatedContent = json_encode($relArray);
+
+        ?>
+
+        <?php $lang = str_replace("{language}", Yii::app()->language, $markup); ?>
+        <?php $viz = str_replace("{vizabi_state}", $vizabi_state, $lang); ?>
+        <?php echo str_replace("{related_content}", $relatedContent, $viz); ?>
+</div>
     <?php
     else: ?>
 
@@ -37,7 +70,5 @@
         <div class="admin-container hide">
             <h3>Developer access</h3>
             <?php echo CHtml::link('<i class="icon-edit"></i> ' . Yii::t('model', 'Update {model}', array('{model}' => Yii::t('model', 'Snapshot'))), array('snapshot/update', 'id' => $data->id, 'returnUrl' => Yii::app()->request->url), array('class' => 'btn')); ?>
-        </div>
-    <?php endif; ?>
-
-</div>
+    </div>
+<?php endif; ?>
