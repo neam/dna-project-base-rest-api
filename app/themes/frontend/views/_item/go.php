8889a7a056a394750a0964cd86e9e65a2fef6f35
Fredrik Wollsén <fredrik@neam.se>
2013-11-27 20:22:12 +0200

    Restored previous sample code

--- app/themes/frontend/views/_item/go.php
+++ app/themes/frontend/views/_item/go.php
@@ -37,3 +37,26 @@ $relatedContent = json_encode($relArray);
     <script>
         relatedPage('container', 'income_mountain', <?= $relatedContent; ?>);
     </script>
+
+<?php /*
+<div class="row-fluid">
+    <div class="span9">
+        <?php $this->renderPartial('_view', array('data' => $model)); ?>
+    </div>
+    <div class="span3">
+        <?php if (count($model->node()->outNodes) == 0): ?>
+            <?php echo Yii::t('go', 'There are no related nodes'); ?>
+        <?php
+        else:
+            foreach ($model->node()->outNodes as $node):
+                var_dump($node->attributes);
+                ?>
+                <?php //$this->renderPartial('_related-view', array('data' => $node->item())); ?>
+            <?php
+            endforeach;
+        endif;
+        ?>
+    </div>
+</div>
+*/
+?>
