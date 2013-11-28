63f4c16c77d8d20f9729504913012a3e2896f1cd
Eric Nishio <eric.nishio@nordsoftware.com>
2013-11-28 10:35:58 +0200

    Add SlugIt to title fields

--- app/themes/frontend/views/dataSource/steps/fields/title.php
+++ app/themes/frontend/views/dataSource/steps/fields/title.php
@@ -1,6 +1,12 @@
-<?php echo $form->textFieldRow($model, 'title_' . $model->source_language, array('maxlength' => 255)); ?>
+<?php echo $form->textFieldRow($model, 'title_' . $model->source_language, array(
+    'maxlength' => 255,
+    'id' => 'slugit-from-1',
+)); ?>
 <?php if ($this->workflowData["translateInto"]) {
-    echo $form->textFieldRow($model, 'title_' . $this->workflowData["translateInto"], array('maxlength' => 255));
+    echo $form->textFieldRow($model, 'title_' . $this->workflowData["translateInto"], array(
+        'maxlength' => 255,
+        'id' => 'slugit-from-2',
+    ));
 } ?>
 <?php if ($model->getAttributeHint("title")): ?>
     <p class="alert alert-info help-block">
@@ -8,12 +14,25 @@
     </p>
 <?php endif; ?>
 
-<?php echo $form->textFieldRow($model, 'slug_' . $model->source_language, array('maxlength' => 255)); ?>
+<?php echo $form->textFieldRow($model, 'slug_' . $model->source_language, array(
+    'maxlength' => 255,
+    'id' => 'slugit-to-1',
+)); ?>
+
 <?php if ($this->workflowData["translateInto"]) {
-    echo $form->textFieldRow($model, 'slug_' . $this->workflowData["translateInto"], array('maxlength' => 255));
+    echo $form->textFieldRow($model, 'slug_' . $this->workflowData["translateInto"], array(
+        'maxlength' => 255,
+        'id' => 'slugit-to-2'
+    ));
 } ?>
+
 <?php if ($model->getAttributeHint("slug")): ?>
     <p class="alert alert-info help-block">
         <?php echo $model->getAttributeHint("slug"); ?>
     </p>
 <?php endif; ?>
+
+<?php Html::jsSlugIt(array(
+    '#slugit-from-1' => '#slugit-to-1',
+    '#slugit-from-2' => '#slugit-to-2',
+)); ?>
--- app/themes/frontend/views/slideshowFile/steps/fields/title.php
+++ app/themes/frontend/views/slideshowFile/steps/fields/title.php
@@ -1,15 +1,35 @@
-<?php echo $form->textFieldRow($model, 'title_en', array('maxlength' => 255)); ?>
+<?php echo $form->textFieldRow($model, 'title_en', array(
+    'maxlength' => 255,
+    'id' => 'slugit-from-1',
+)); ?>
+
 <?php if ($this->workflowData["translateInto"]) {
-    echo $form->textFieldRow($model, 'title_' . $this->workflowData["translateInto"], array('maxlength' => 255));
+    echo $form->textFieldRow($model, 'title_' . $this->workflowData["translateInto"], array(
+        'maxlength' => 255,
+        'id' => 'slugit-from-2',
+    ));
 } ?>
 <p class="alert alert-info help-block">
     <?php echo $model->getAttributeHint("title"); ?>
 </p>
 
-<?php echo $form->textFieldRow($model, 'slug_en', array('maxlength' => 255)); ?>
+<?php echo $form->textFieldRow($model, 'slug_en', array(
+    'maxlength' => 255,
+    'id' => 'slugit-to-1',
+)); ?>
+
 <?php if ($this->workflowData["translateInto"]) {
-    echo $form->textFieldRow($model, 'slug_' . $this->workflowData["translateInto"], array('maxlength' => 255));
+    echo $form->textFieldRow($model, 'slug_' . $this->workflowData["translateInto"], array(
+        'maxlength' => 255,
+        'id' => 'slugit-to-2',
+    ));
 } ?>
+
 <p class="alert alert-info help-block">
     <?php echo $model->getAttributeHint("slug"); ?>
 </p>
+
+<?php Html::jsSlugIt(array(
+    '#slugit-from-1' => '#slugit-to-1',
+    '#slugit-from-2' => '#slugit-to-2',
+)); ?>
