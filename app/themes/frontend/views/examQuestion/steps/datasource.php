8d1d2643793e89012ffc9c91a9f4ffc21e30fc85
Eric Nishio <eric.nishio@nordsoftware.com>
2013-11-28 16:39:47 +0200

    Add attribute hints as tooltips to ExamQuestion fields

--- app/themes/frontend/views/examQuestion/steps/datasource.php
+++ app/themes/frontend/views/examQuestion/steps/datasource.php
@@ -1,46 +1,36 @@
-<?php
-$input = $this->widget(
-    '\GtcRelation',
-    array(
-        'model' => $model,
-        'relation' => 'sourceNode',
-        'fields' => 'itemLabel',
-        'allowEmpty' => true,
-        'style' => 'dropdownlist',
-        'htmlOptions' => array(
-            'checkAll' => 'all'
-        ),
-    )
-    , true);
-echo $form->customRow($model, 'data_source_id', $input);
-?>
+<?php $input = $this->widget('\GtcRelation', array(
+    'model' => $model,
+    'relation' => 'sourceNode',
+    'fields' => 'itemLabel',
+    'allowEmpty' => true,
+    'style' => 'dropdownlist',
+    'htmlOptions' => array(
+        'checkAll' => 'all'
+    ),
+), true); ?>
 
-    <div class="control-group">
-        <div class="controls">
-            <?php
-            echo $this->widget('bootstrap.widgets.TbButton', array(
-                'label' => Yii::t('app', 'Add Data source'),
-                'icon' => 'icon-plus',
-                'htmlOptions' => array(
-                    'data-toggle' => 'modal',
-                    'data-target' => '#addrelation-examquestion-datasource-modal',
-                ),
-            ), true);
-            ?>
-        </div>
+<?php echo $form->customRow($model, 'data_source_id', $input, array(
+    'labelOptions' => array(
+        'label' => Html::attributeLabelWithTooltip($model, 'data_source_id', 'datasource'),
+    ),
+)); ?>
+
+<div class="control-group">
+    <div class="controls">
+        <?php echo $this->widget('bootstrap.widgets.TbButton', array(
+            'label' => Yii::t('app', 'Add Data source'),
+            'icon' => 'icon-plus',
+            'htmlOptions' => array(
+                'data-toggle' => 'modal',
+                'data-target' => '#addrelation-examquestion-datasource-modal',
+            ),
+        ), true); ?>
     </div>
+</div>
 
-<?php if ($model->getAttributeHint("datasource")): ?>
-    <p class="alert alert-info help-block">
-        <?php echo $model->getAttributeHint("datasource"); ?>
-    </p>
-<?php endif; ?>
-
-<?php
-$this->renderPartial('//gridRelation/_modal_form_single', array(
+<?php $this->renderPartial('//gridRelation/_modal_form_single', array(
     'model' => $model,
     'toType' => 'DataSource',
     'toLabel' => 'data source',
     'inputId' => 'data_source_id',
-));
-?>
+)); ?>
--- app/themes/frontend/views/examQuestion/steps/fields/question.php
+++ app/themes/frontend/views/examQuestion/steps/fields/question.php
@@ -1,17 +1,14 @@
 <?php echo $form->textFieldRow($model, 'question_' . $model->source_language, array(
-    'maxlength' => 200,
     'id' => 'slugit-from-1',
+    'maxlength' => 200,
+    'labelOptions' => array(
+        'label' => Html::attributeLabelWithTooltip($model, 'question_' . $model->source_language, 'question'),
+    ),
 )); ?>
 
-<?php if ($this->workflowData["translateInto"]) {
-    echo $form->textFieldRow($model, 'question_' . $this->workflowData["translateInto"], array(
-        'maxlength' => 255,
+<?php if ($this->workflowData['translateInto']) {
+    echo $form->textFieldRow($model, 'question_' . $this->workflowData['translateInto'], array(
         'id' => 'slugit-from-2',
+        'maxlength' => 255,
     ));
 } ?>
-
-<?php if ($model->getAttributeHint("question")): ?>
-    <p class="alert alert-info help-block">
-        <?php echo $model->getAttributeHint("question"); ?>
-    </p>
-<?php endif; ?>
