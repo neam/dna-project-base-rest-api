15037229b403ce7551b15a1d9af85bce54fdf5c7
Fredrik Wollsén <fredrik@neam.se>
2014-01-21 17:48:36 +0200

    Site index clean-up

--- app/themes/frontend/views/site/index.php
+++ app/themes/frontend/views/site/index.php
@@ -1,56 +1,23 @@
-<!--
-<h1>
-    Gapminder CMS
-</h1>
-
-<h2>
-    Search
-</h2>
-
-<div class="alert alert-warning">
-    TODO: Ability to search
-</div>
-
-<h2>
-    Featured
-</h2>
-
-<div class="alert alert-warning">
-    TODO: See featured items
-</div>
-
-<h2>
-    Contents
-</h2>
--->
 <h2><?php print Yii::t('app', 'Go-items'); ?></h2>
+
 <?php foreach (DataModel::goItemModels() as $modelClass => $table):
 
-    if ($table == "exam_question_alternative") {
-        continue;
-    }
     $this->renderPartial("_item-type-introduction", compact("modelClass"));
 
 endforeach; ?>
 
-<h2><?php print Yii::t('app', 'Educational Content'); ?></h2>
+<h2><?php print Yii::t('app', 'Educational material'); ?></h2>
 
 <?php foreach (DataModel::educationalContentModels() as $modelClass => $table):
 
-    if ($table == "exam_question_alternative") {
-        continue;
-    }
     $this->renderPartial("_item-type-introduction", compact("modelClass"));
 
 endforeach; ?>
 
-<h2><?php print Yii::t('app', 'Website Content'); ?></h2>
+<h2><?php print Yii::t('app', 'Website content'); ?></h2>
 
 <?php foreach (DataModel::websiteContentModels() as $modelClass => $table):
 
-    if ($table == "exam_question_alternative") {
-        continue;
-    }
     $this->renderPartial("_item-type-introduction", compact("modelClass"));
 
 endforeach; ?>
