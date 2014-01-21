77f39a72e3513fab296a288932cdedb34cca494d
Fredrik Wollsén <fredrik@neam.se>
2014-01-21 19:12:44 +0200

    Dashboard clean-up and preparation for showing translation tasks

--- app/themes/frontend/views/account/dashboard.php
+++ app/themes/frontend/views/account/dashboard.php
@@ -14,68 +14,46 @@ $this->breadcrumbs[] = Yii::t('account', 'Dashboard');
 
 <?php $this->renderPartial("_toolbar", array("model" => $model)); ?>
 
-<?php
-$roles = array_keys(Yii::app()->authManager->getAuthItems(2, $model->id));
-?>
-
-<h2>
-    <?php echo Yii::t('dashboard', 'Quick start'); ?>
-</h2>
-
-<?php if (empty($roles)): ?>
+<?php if (!Yii::app()->user->checkAccess('Item.Translate') && !Yii::app()->user->checkAccess('Item.Edit') && !Yii::app()->user->checkAccess('Item.Add')): ?>
 
     <div class="alert alert-error">
-        <?php echo Yii::t('account', 'You do not have any permissions assigned.'); ?>
+        <?php echo Yii::t('account', 'You do not have permissions assigned.'); ?>
         <?php echo Yii::t('account', 'Without any permissions, we can not propose a suitable task for you.'); ?>
         <?php echo Yii::t('account', 'Only administrators can assign permissions.'); ?>
         <?php //echo Yii::t('account', 'Please apply for a permission.'); ?>
     </div>
 
-<?php else: ?>
+<?php endif; ?>
 
+<?php if (Yii::app()->user->checkAccess('Item.Add')): ?>
+
+    <h2><?php echo Yii::t('dashboard', 'Add contents'); ?></h2>
     <?php foreach (DataModel::qaModels() as $modelClass => $table): ?>
 
         <?php if ($table == "exam_question_alternative") {
             continue;
         } ?>
 
-        <?php if (Yii::app()->user->checkAccess('Item.Add')): ?>
-
-            <?php
-            $this->widget("bootstrap.widgets.TbButton", array(
-                "label" => Yii::t("model", "List {$modelClass}s"),
-                "size" => "large",
-                "icon" => "icon-globe",
-                "url" => array(lcfirst($modelClass) . "/index")
-            ));
-            ?>
-
-        <?php endif; ?>
+        <?php
+        $this->widget("bootstrap.widgets.TbButton", array(
+            "label" => Yii::t("model", "Create $modelClass"),
+            "size" => "large",
+            "icon" => "icon-plus",
+            "url" => array(lcfirst($modelClass) . "/add")
+        ));
+        ?>
 
     <?php endforeach; ?>
 
-    <hr>
+<?php endif; ?>
 
-    <?php foreach (DataModel::qaModels() as $modelClass => $table): ?>
+<?php if (Yii::app()->user->checkAccess('Item.Translate')): ?>
 
-        <?php if ($table == "exam_question_alternative") {
-            continue;
-        } ?>
+    <h2><?php echo Yii::t('dashboard', 'Translate contents'); ?></h2>
 
-        <?php if (Yii::app()->user->checkAccess('Item.Add')): ?>
-
-            <?php
-            $this->widget("bootstrap.widgets.TbButton", array(
-                "label" => Yii::t("model", "Create $modelClass"),
-                "size" => "large",
-                "icon" => "icon-plus",
-                "url" => array(lcfirst($modelClass) . "/add")
-            ));
-            ?>
-
-        <?php endif; ?>
-
-    <?php endforeach; ?>
+    <div class="alert alert-error">
+        Translation tasks TODO list here
+    </div>
 
 <?php endif; ?>
 
