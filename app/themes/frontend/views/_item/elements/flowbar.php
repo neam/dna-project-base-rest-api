9743f0612ffe0d5ff44647cd7eb02be003dc2ebd
Fredrik Wollsén <fredrik@neam.se>
2014-01-22 19:05:09 +0200

    Only showing buttons to flows that the user has access to

--- app/themes/frontend/views/_item/elements/flowbar.php
+++ app/themes/frontend/views/_item/elements/flowbar.php
@@ -93,7 +93,8 @@
                             "label" => Yii::t("crud", "Prepare for testing"),
                             "type" => $this->action->id == "prepPreshow" ? "inverse" : null,
                             "icon" => "icon-pencil" . ($this->action->id == "" ? " icon-white" : null),
-                            "url" => array("prepPreshow", "id" => $model->{$model->tableSchema->primaryKey}, "step" => $this->firstFlowStep($model))
+                            "url" => array("prepPreshow", "id" => $model->{$model->tableSchema->primaryKey}, "step" => $this->firstFlowStep($model)),
+                            "visible" => Yii::app()->user->checkAccess('Item.PrepPreshow'),
                         ));
                     }
                     if (!$model->qaState()->candidate_for_public_status) {
@@ -101,7 +102,8 @@
                             "label" => Yii::t("crud", "Prepare for publishing"),
                             "type" => $this->action->id == "prepPublish" ? "inverse" : null,
                             "icon" => "icon-pencil" . ($this->action->id == "prepPublish" ? " icon-white" : null),
-                            "url" => array("prepPublish", "id" => $model->{$model->tableSchema->primaryKey}, "step" => $this->firstFlowStep($model))
+                            "url" => array("prepPublish", "id" => $model->{$model->tableSchema->primaryKey}, "step" => $this->firstFlowStep($model)),
+                            "visible" => Yii::app()->user->checkAccess('Item.PrepPublish'),
                         ));
                     }
 
@@ -114,25 +116,29 @@
                         "label" => Yii::t("crud", "Evaluate"),
                         "type" => $this->action->id == "evaluate" ? "inverse" : null,
                         "icon" => "icon-comment" . ($this->action->id == "evaluate" ? " icon-white" : null),
-                        "url" => array("evaluate", "id" => $model->{$model->tableSchema->primaryKey}, "step" => $this->firstFlowStep($model))
+                        "url" => array("evaluate", "id" => $model->{$model->tableSchema->primaryKey}, "step" => $this->firstFlowStep($model)),
+                        "visible" => Yii::app()->user->checkAccess('Item.Evaluate'),
                     ));
                     $this->widget("bootstrap.widgets.TbButton", array(
                         "label" => Yii::t("model", "Review"),
                         "type" => $this->action->id == "review" ? "inverse" : null,
                         "icon" => "icon-check" . ($this->action->id == "review" ? " icon-white" : null),
-                        "url" => array("review", "id" => $model->{$model->tableSchema->primaryKey})
+                        "url" => array("review", "id" => $model->{$model->tableSchema->primaryKey}),
+                        "visible" => Yii::app()->user->checkAccess('Item.Review'),
                     ));
                     $this->widget("bootstrap.widgets.TbButton", array(
                         "label" => Yii::t("model", "Proofread"),
                         "type" => $this->action->id == "proofRead" ? "inverse" : null,
                         "icon" => "icon-certificate" . ($this->action->id == "proofRead" ? " icon-white" : null),
-                        "url" => array("proofRead", "id" => $model->{$model->tableSchema->primaryKey})
+                        "url" => array("proofRead", "id" => $model->{$model->tableSchema->primaryKey}),
+                        "visible" => Yii::app()->user->checkAccess('Item.Proofread'),
                     ));
                     $this->widget("bootstrap.widgets.TbButton", array(
                         "label" => Yii::t("model", "Translate"),
                         "type" => $this->action->id == "translationOverview" ? "inverse" : null,
                         "icon" => "icon-globe" . ($this->action->id == "translationOverview" ? " icon-white" : null),
-                        "url" => array("translationOverview", "id" => $model->{$model->tableSchema->primaryKey})
+                        "url" => array("translationOverview", "id" => $model->{$model->tableSchema->primaryKey}),
+                        "visible" => Yii::app()->user->checkAccess('Item.Translate'),
                     ));
 
                     ?>
@@ -144,19 +150,22 @@
                         $this->widget("bootstrap.widgets.TbButton", array(
                             "label" => Yii::t("model", "Unpublish"),
                             "icon" => "icon-thumbs-down",
-                            "url" => array("unpublish", "id" => $model->{$model->tableSchema->primaryKey})
+                            "url" => array("unpublish", "id" => $model->{$model->tableSchema->primaryKey}),
+                            "visible" => Yii::app()->user->checkAccess('Item.Publish'),
                         ));
                     } elseif ($model->qaStateBehavior()->validStatus('public')) {
                         $this->widget("bootstrap.widgets.TbButton", array(
                             "label" => Yii::t("model", "Publish"),
                             "icon" => "icon-thumbs-up",
-                            "url" => array("publish", "id" => $model->{$model->tableSchema->primaryKey})
+                            "url" => array("publish", "id" => $model->{$model->tableSchema->primaryKey}),
+                            "visible" => Yii::app()->user->checkAccess('Item.Publish'),
                         ));
                     } else {
                         $this->widget("bootstrap.widgets.TbButton", array(
                             "label" => Yii::t("model", "Publish"),
                             "disabled" => true,
                             "icon" => "icon-thumbs-up",
+                            "visible" => Yii::app()->user->checkAccess('Item.Publish'),
                         ));
                     }
 
@@ -169,19 +178,22 @@
                         "label" => Yii::t("model", "Clone"),
                         "type" => $this->action->id == "clone" ? "inverse" : null,
                         "icon" => "icon-plus" . ($this->action->id == "clone" ? " icon-white" : null),
-                        "url" => array("clone", "id" => $model->{$model->tableSchema->primaryKey})
+                        "url" => array("clone", "id" => $model->{$model->tableSchema->primaryKey}),
+                        "visible" => Yii::app()->user->checkAccess('Item.Clone'),
                     ));
                     $this->widget("bootstrap.widgets.TbButton", array(
                         "label" => Yii::t("model", "Remove"),
                         "type" => $this->action->id == "remove" ? "inverse" : null,
                         "icon" => "icon-eye-close" . ($this->action->id == "remove" ? " icon-white" : null),
-                        "url" => array("remove", "id" => $model->{$model->tableSchema->primaryKey})
+                        "url" => array("remove", "id" => $model->{$model->tableSchema->primaryKey}),
+                        "visible" => Yii::app()->user->checkAccess('Item.Remove'),
                     ));
                     $this->widget("bootstrap.widgets.TbButton", array(
                         "label" => Yii::t("model", "Replace"),
                         "type" => $this->action->id == "replace" ? "inverse" : null,
                         "icon" => "icon-random" . ($this->action->id == "replace" ? " icon-white" : null),
-                        "url" => array("replace", "id" => $model->{$model->tableSchema->primaryKey})
+                        "url" => array("replace", "id" => $model->{$model->tableSchema->primaryKey}),
+                        "visible" => Yii::app()->user->checkAccess('Item.Replace'),
                     ));
 
                     ?>
