cffc9c66a0372767173086140ad7ec4b3ea3fd23
Fredrik Wollsén <fredrik@neam.se>
2014-01-22 10:28:06 +0200

    CMS-365 (Hide the Translations section for now) #resolve

--- app/themes/frontend/views/account/_toolbar.php
+++ app/themes/frontend/views/account/_toolbar.php
@@ -25,12 +25,14 @@
                 "type" => $this->action->id == "dashboard" ? "inverse" : null,
                 "url" => array("dashboard")
             ));
+            /*
             $this->widget("bootstrap.widgets.TbButton", array(
                 "label" => Yii::t("model", "Translations"),
                 "icon" => "icon-globe" . ($this->action->id == "translations" ? " icon-white" : null),
                 "type" => $this->action->id == "translations" ? "inverse" : null,
                 "url" => array("translations")
             ));
+            */
             $this->widget("bootstrap.widgets.TbButton", array(
                 "label" => Yii::t("model", "Profile"),
                 "icon" => "icon-user" . ($this->action->id == "profile" ? " icon-white" : null),
