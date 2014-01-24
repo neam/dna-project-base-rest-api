810dd626333d9a30e088da842af72af08c8acd1d
Fredrik Wollsén <fredrik@neam.se>
2014-01-24 13:21:12 +0200

    Added status and progress fields to item view

--- app/commands/DatabaseViewGeneratorCommand.php
+++ app/commands/DatabaseViewGeneratorCommand.php
@@ -53,12 +53,31 @@ WHERE
 
     }
 
+    /**
+     * item
+     *      node_id
+     *      id
+     *      _title
+     *      status
+     *      preview_validation_progress
+     *      public_validation_progress
+     *      approval_progress
+     *      proofing_progress
+     *      translate_into_{$language}_validation_progress
+     *      model_class
+     *      item_type
+     *      created
+     *      modified
+     */
     public function actionItem()
     {
 
         $sql = "SELECT \n";
+
+        // node_id
         $sql .= "   node.id as node_id,\n";
 
+        // id
         $sql .= "   CASE\n";
         foreach (DataModel::qaModels() as $modelClass => $table) {
             if ($this->_checkTableAndColumnExists($table, "id")) {
@@ -68,17 +87,7 @@ WHERE
         $sql .= "       ELSE NULL\n";
         $sql .= "END AS id,\n";
 
-        if (false) {
-            foreach (DataModel::qaModels() as $modelClass => $table) {
-
-                $sql .= "   `$table`.`id` AS `$modelClass.id`,\n";
-                if ($this->_checkTableAndColumnExists($table, "_title")) {
-                    $sql .= "   `$table`.`_title` AS `$modelClass._title`,\n";
-                }
-
-            }
-        }
-
+        // _title
         $sql .= "   CASE\n";
 
         foreach (DataModel::qaModels() as $modelClass => $table) {
@@ -90,6 +99,83 @@ WHERE
         $sql .= "       ELSE NULL\n";
         $sql .= "END AS _title,\n";
 
+        // status
+        $sql .= "   CASE\n";
+
+        foreach (DataModel::qaModels() as $modelClass => $table) {
+            if ($this->_checkTableAndColumnExists($table . "_qa_state", "status")) {
+                $sql .= "       WHEN {$table}_qa_state.status IS NOT NULL THEN {$table}_qa_state.status\n";
+            }
+        }
+
+        $sql .= "       ELSE NULL\n";
+        $sql .= "END AS status,\n";
+
+        // preview_validation_progress
+        $sql .= "   CASE\n";
+
+        foreach (DataModel::qaModels() as $modelClass => $table) {
+            if ($this->_checkTableAndColumnExists($table . "_qa_state", "preview_validation_progress")) {
+                $sql .= "       WHEN {$table}_qa_state.preview_validation_progress IS NOT NULL THEN {$table}_qa_state.preview_validation_progress\n";
+            }
+        }
+
+        $sql .= "       ELSE NULL\n";
+        $sql .= "END AS preview_validation_progress,\n";
+
+        // public_validation_progress
+        $sql .= "   CASE\n";
+
+        foreach (DataModel::qaModels() as $modelClass => $table) {
+            if ($this->_checkTableAndColumnExists($table . "_qa_state", "public_validation_progress")) {
+                $sql .= "       WHEN {$table}_qa_state.public_validation_progress IS NOT NULL THEN {$table}_qa_state.public_validation_progress\n";
+            }
+        }
+
+        $sql .= "       ELSE NULL\n";
+        $sql .= "END AS public_validation_progress,\n";
+
+        // approval_progress
+        $sql .= "   CASE\n";
+
+        foreach (DataModel::qaModels() as $modelClass => $table) {
+            if ($this->_checkTableAndColumnExists($table . "_qa_state", "approval_progress")) {
+                $sql .= "       WHEN {$table}_qa_state.approval_progress IS NOT NULL THEN {$table}_qa_state.approval_progress\n";
+            }
+        }
+
+        $sql .= "       ELSE NULL\n";
+        $sql .= "END AS approval_progress,\n";
+
+        // proofing_progress
+        $sql .= "   CASE\n";
+
+        foreach (DataModel::qaModels() as $modelClass => $table) {
+            if ($this->_checkTableAndColumnExists($table . "_qa_state", "proofing_progress")) {
+                $sql .= "       WHEN {$table}_qa_state.proofing_progress IS NOT NULL THEN {$table}_qa_state.proofing_progress\n";
+            }
+        }
+
+        $sql .= "       ELSE NULL\n";
+        $sql .= "END AS proofing_progress,\n";
+
+        // translate_into_{$language}_validation_progress
+        foreach (Yii::app()->params["languages"] as $language => $label) {
+
+            $sql .= "   CASE\n";
+
+            foreach (DataModel::qaModels() as $modelClass => $table) {
+                if ($this->_checkTableAndColumnExists($table . "_qa_state", "translate_into_{$language}_validation_progress")) {
+                    $sql .= "       WHEN {$table}_qa_state.translate_into_{$language}_validation_progress IS NOT NULL THEN {$table}_qa_state.translate_into_{$language}_validation_progress\n";
+                }
+            }
+
+            $sql .= "       ELSE NULL\n";
+            $sql .= "END AS translate_into_{$language}_validation_progress,\n";
+
+        }
+
+        // model_class
         $sql .= "   CASE\n";
 
         foreach (DataModel::qaModels() as $modelClass => $table) {
@@ -99,6 +185,7 @@ WHERE
         $sql .= "       ELSE NULL\n";
         $sql .= "END AS model_class,\n";
 
+        // item_type
         $sql .= "   CASE\n";
 
         foreach (DataModel::goItemModels() as $modelClass => $table) {
@@ -117,12 +204,36 @@ WHERE
         */
 
         $sql .= "       ELSE NULL\n";
-        $sql .= "END AS item_type\n";
+        $sql .= "END AS item_type,\n";
+
+        // created
+        $sql .= "   CASE\n";
+
+        foreach (DataModel::qaModels() as $modelClass => $table) {
+            if ($this->_checkTableAndColumnExists($table, "created")) {
+                $sql .= "       WHEN $table.created IS NOT NULL THEN $table.created\n";
+            }
+        }
+
+        $sql .= "       ELSE NULL\n";
+        $sql .= "END AS created,\n";
+
+        // created
+        $sql .= "   CASE\n";
+
+        foreach (DataModel::qaModels() as $modelClass => $table) {
+            if ($this->_checkTableAndColumnExists($table, "modified")) {
+                $sql .= "       WHEN $table.modified IS NOT NULL THEN $table.modified\n";
+            }
+        }
+
+        $sql .= "       ELSE NULL\n";
+        $sql .= "END AS modified\n";
 
         $sql .= "FROM node\n";
 
         foreach (DataModel::qaModels() as $modelClass => $table) {
-            $sql .= "       LEFT JOIN $table ON $table.node_id = node.id\n";
+            $sql .= "       LEFT JOIN ($table INNER JOIN {$table}_qa_state ON {$table}.{$table}_qa_state_id = {$table}_qa_state.id) ON $table.node_id = node.id \n";
         }
 
         $viewName = "item";
