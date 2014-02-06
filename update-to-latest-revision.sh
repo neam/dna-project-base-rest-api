ab6bc312e1fb3fb50d87772ac802456aebb82b83
Fredrik Wollsén <fredrik@neam.se>
2014-02-06 17:17:55 +0200

    Removed proof-of-concept sample generated view and added generation of item view to update script

--- update-to-latest-revision.sh
+++ update-to-latest-revision.sh
@@ -6,4 +6,5 @@ rm -r www/assets/*
 app/yiic migrate
 app/yiic authorizationhierarchy reset
 app/yiic authorizationhierarchy build
+app/yiic databaseviewgenerator item
 echo "Script finished. Don't forget to synchronize the db schema using MySQL Workbench"
