4fd9cd82598f89cb3e98f742464b8857a8878f1f
Eric Nishio <eric.nishio@nordsoftware.com>
2014-02-07 10:43:02 +0200

    CMS-473 Show nothing but an error message on the translation page if there are no subtitles

--- app/controllers/traits/ItemController.php
+++ app/controllers/traits/ItemController.php
@@ -837,11 +837,16 @@ trait ItemController
         $this->scenario = "into_$translateInto-step_$step";
         $model = $this->loadModel($id);
         $model->scenario = $this->scenario;
-        $this->performAjaxValidation($model);
-        $this->saveAndContinueOnSuccess($model);
-        $this->populateWorkflowData($model, "translate", Yii::t('app', 'Translate into {translateIntoLanguage}', array('{translateIntoLanguage}' => Yii::app()->params["languages"][$translateInto])), $translateInto);
-        $stepCaptions = $model->flowStepCaptions();
-        $this->render('/_item/edit', array('model' => $model, 'step' => $step, 'stepCaption' => $stepCaptions[$step]));
+        $subtitles = $model->getParsedSubtitles();
+        if (!empty($subtitles)) {
+            $this->performAjaxValidation($model);
+            $this->saveAndContinueOnSuccess($model);
+            $this->populateWorkflowData($model, "translate", Yii::t('app', 'Translate into {translateIntoLanguage}', array('{translateIntoLanguage}' => Yii::app()->params["languages"][$translateInto])), $translateInto);
+            $stepCaptions = $model->flowStepCaptions();
+            $this->render('/_item/edit', array('model' => $model, 'step' => $step, 'stepCaption' => $stepCaptions[$step]));
+        } else {
+            throw new CHttpException(404, Yii::t('error', 'No subtitles found.'));
+        }
     }
 
     public $workflowData = array();
