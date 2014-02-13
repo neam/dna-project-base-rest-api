36a98c587c4b4d65a535b9adb1de9ab88121d08d
Fredrik Wollsén <fredrik@neam.se>
2014-02-13 15:25:49 +0000

    Added permissions changes to update script

--- update-to-latest-revision.sh
+++ update-to-latest-revision.sh
@@ -3,6 +3,12 @@ npm install
 bower install --allow-root
 php composer.phar install
 rm -r www/assets/*
+chown -R www-data:www-data app/data/
+chown -R www-data:www-data app/runtime/
+chown -R www-data:www-data www/assets/
+chmod -R g+rw app/data/
+chmod -R g+rw app/runtime/
+chmod -R g+rw www/assets/
 app/yiic migrate
 app/yiic authorizationhierarchy reset
 app/yiic authorizationhierarchy build
