109e693d564302c61a9aefa2e20f8ec410005b6d
Eric Nishio <eric.nishio@nordsoftware.com>
2014-02-26 14:24:12 +0200

    Refactor data source edit

---
+++ app/themes/gapminder/views/dataSource/steps/fields/about.php
@@ -0,0 +1,26 @@
+<?php
+/* @var DataSourceController|ItemController $this */
+/* @var DataSource|ItemTrait $model */
+/* @var AppActiveForm|TbActiveForm $form */
+?>
+<?php echo $form->textAreaControlGroup(
+    $model,
+    'about_' . $model->source_language,
+    array(
+        'class' => Html::ITEM_FORM_FIELD_CLASS,
+        'rows' => 6,
+        'cols' => 50,
+        'labelOptions' => array(
+            'label' => Html::attributeLabelWithTooltip($model, 'about_' . $model->source_language, 'about'),
+        ),
+    )
+); ?>
+<?php if ($this->workflowData['translateInto']): ?>
+    <?php echo $form->textAreaControlGroup(
+        $model, 'about_' . $this->workflowData['translateInto'], array(
+            'class' => Html::ITEM_FORM_FIELD_CLASS,
+            'rows' => 6,
+            'cols' => 50,
+        )
+    ); ?>
+<?php endif; ?>
