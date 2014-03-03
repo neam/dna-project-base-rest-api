919b56be4ba3f3293d46b92ee71c40b1f77938ac
Eric Nishio <eric.nishio@nordsoftware.com>
2014-03-03 17:15:46 +0200

    Add Upload button to VideoFile/edit

--- app/themes/gapminder/views/videofile/steps/files.php
+++ app/themes/gapminder/views/videofile/steps/files.php
@@ -4,3 +4,24 @@
 /* @var AppActiveForm|TbActiveForm $form */
 ?>
 <?php echo $form->select2ControlGroup($model, 'original_media_id', $model->getRelatedVideoOptions()); ?>
+<?php echo TbHtml::button(
+    Yii::t('app', 'Upload'),
+    array(
+        'icon' => TbHtml::ICON_PLUS,
+        'class' => 'upload-btn',
+        'data-toggle' => 'modal',
+        'data-target' => '#' . $form->id . '-modal',
+    )
+); ?>
+<?php $this->beginClip('modal:' . $form->id . '-modal'); ?>
+    <?php $this->renderPartial(
+        '//p3Media/_modal_form',
+        array(
+            'formId' => $form->id,
+            'inputSelector' => "#VideoFile_original_media_id",
+            'model' => new P3Media(),
+            'pk' => 'id',
+            'field' => 'itemLabel',
+        )
+    ); ?>
+<?php $this->endClip(); ?>
