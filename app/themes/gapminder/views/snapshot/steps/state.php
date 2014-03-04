bfa4f7797421144c2aee13d6e1ecb024a41478db
Jimi Pirilä <jimi.pirila@nordsoftware.com>
2014-03-04 13:39:11 +0200

    Implement Snapshot tool-input as a select2-field

--- app/themes/gapminder/views/snapshot/steps/state.php
+++ app/themes/gapminder/views/snapshot/steps/state.php
@@ -3,8 +3,7 @@
 
 $this->renderPartial("steps/fields/vizabi_state", compact("form", "model"));
 
-// todo: implement the add tool modal properly
-//$this->renderPartial("steps/fields/tool", compact("form", "model"));
+$this->renderPartial("steps/fields/tool", compact("form", "model"));
 
 $this->renderPartial("steps/fields/embed_override", compact("form", "model"));
 
