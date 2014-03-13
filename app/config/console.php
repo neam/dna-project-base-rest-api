51717ddf912c0d437ebd3ec68010586b518c3c3e
Fredrik Wollsén <fredrik@neam.se>
2014-03-13 17:25:43 -0500

    Cleaned up all active migrations so that only useful migrations are applied in a database with recent schema dump

--- app/config/console.php
+++ app/config/console.php
@@ -47,14 +47,15 @@ $consoleConfig = array(
             'applicationModuleName' => 'core',
             // define all available modules (if you do not set this, modules will be set from yii app config)
             'modulePaths'           => array(
-                'rights'                => 'application.migrations.rights',
                 'user'                  => 'vendor.mishamx.yii-user.migrations',
-                'p3pages'               => 'vendor.phundament.p3pages.migrations',
-                'p3widgets'             => 'vendor.phundament.p3widgets.migrations',
-                'p3media'               => 'vendor.phundament.p3media.migrations',
-                'ckeditor-configurator' => 'vendor.schmunk42.ckeditor-configurator.migrations',
+                // The following migrations are not used since they are already included in the data model or the extension is not used
+                //'rights'                => 'application.migrations.rights',
+                //'p3pages'               => 'vendor.phundament.p3pages.migrations',
+                //'p3widgets'             => 'vendor.phundament.p3widgets.migrations',
+                //'p3media'               => 'vendor.phundament.p3media.migrations',
+                //'ckeditor-configurator' => 'vendor.schmunk42.ckeditor-configurator.migrations',
                 //'translate'             => 'vendor.gusnips.yii-translate.migrations',
-                'auditrail2'            => 'vendor.sammaye.auditrail2.migrations',
+                //'auditrail2'            => 'vendor.sammaye.auditrail2.migrations',
             ),
             // you can customize the modules migrations subdirectory which is used when you are using yii module config
             'migrationSubPath'      => 'migrations',
