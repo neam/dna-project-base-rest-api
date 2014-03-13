81dfa5c6dbbe3641594e9f3ff08ede25838780e4
Eric Nishio <eric.nishio@nordsoftware.com>
2014-03-13 15:07:22 +0200

    Minor visual fix to video upload field

--- app/themes/gapminder/views/videoFile/steps/fields/thumbnail.php
+++ app/themes/gapminder/views/videoFile/steps/fields/thumbnail.php
@@ -2,23 +2,33 @@
 /* @var VideoFileController|ItemController $this */
 /* @var VideoFile|ItemTrait $model */
 ?>
-<?php echo $form->select2ControlGroup($model, 'thumbnail_media_id', $model->getThumbnailOptions()); ?>
-<?php echo TbHtml::button(
-    Yii::t('app', 'Upload'),
-    array(
-        'icon' => TbHtml::ICON_PLUS,
-        'class' => 'upload-btn',
-        'data-toggle' => 'modal',
-        'data-target' => '#' . $form->id . '-modal',
-    )
-); ?>
-<?php $this->renderPartial(
-    '//p3Media/_modal_form',
-    array(
-        'formId' => $form->id,
-        'inputSelector' => '#VideoFile_thumbnail_media_id',
-        'model' => new P3Media(),
-        'pk' => 'id',
-        'field' => 'itemLabel',
-    )
-); ?>
+<div class="file-field-2cols">
+    <div class="field-column">
+        <?php echo $form->select2ControlGroup($model, 'thumbnail_media_id', $model->getThumbnailOptions()); ?>
+    </div>
+    <div class="field-column">
+        <div class="form-group">
+            <label class="control-label"><?php echo Yii::t('account', '&nbsp;'); ?></label>
+            <?php echo TbHtml::button(
+                Yii::t('app', 'Upload'),
+                array(
+                    'block' => true,
+                    'icon' => TbHtml::ICON_PLUS,
+                    'class' => 'upload-btn',
+                    'data-toggle' => 'modal',
+                    'data-target' => '#' . $form->id . '-modal',
+                )
+            ); ?>
+            <?php $this->renderPartial(
+                '//p3Media/_modal_form',
+                array(
+                    'formId' => $form->id,
+                    'inputSelector' => '#VideoFile_thumbnail_media_id',
+                    'model' => new P3Media(),
+                    'pk' => 'id',
+                    'field' => 'itemLabel',
+                )
+            ); ?>
+        </div>
+    </div>
+</div>
