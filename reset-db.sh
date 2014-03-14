855414618f773333b526eba84dec38f04afd70f9
Fredrik Wollsén <fredrik@neam.se>
2014-03-14 14:06:59 -0400

    Also trashing p3media data when resetting main db

--- reset-db.sh
+++ reset-db.sh
@@ -6,6 +6,8 @@ set -x;
 app/yiic databaseschema --connectionID=db dropAllTablesAndViews --verbose=0
 app/yiic databaseschema --connectionID=db loadSql --path=db/schema.sql --verbose=0
 app/yiic migrate --connectionID=db --interactive=0
+mkdir .trashed-p3media-data
+mv app/data/p3media/* .trashed-p3media-data/
 export connectionID=db
 ./_provision.sh
 
