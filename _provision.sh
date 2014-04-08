7cd288b1e4ade611b6a861971ab69ab99affe757
Fredrik Wollsén <fredrik@neam.se>
2014-04-08 18:00:55 +0300

    First buildpack-related batch of configuration

--- _provision.sh
+++ _provision.sh
@@ -1,23 +1,7 @@
 set -x
 
-if [ "$connectionID" == "" ]; then
-    connectionID=db
-fi
-
 npm install
 bower install --allow-root
 php composer.phar install
-rm -r www/assets/*
-chown -R www-data:www-data app/data/
-chown -R www-data:www-data app/runtime/
-chown -R www-data:www-data www/assets/
-chown -R www-data:www-data www/runtime/
-chmod -R g+rw app/data/
-chmod -R g+rw app/runtime/
-chmod -R g+rw www/assets/
-chmod -R g+rw www/runtime/
-app/yiic migrate --connectionID=$connectionID
-app/yiic authorizationhierarchy reset
-app/yiic authorizationhierarchy build
-app/yiic databaseviewgenerator --connectionID=$connectionID item
-app/yiic fixture --connectionID=$connectionID load
+
+./post-buildstep.sh
---
+++ post-buildstep.sh
@@ -0,0 +1,17 @@
+if [ "$connectionID" == "" ]; then
+    connectionID=db
+fi
+rm -r www/assets/*
+chown -R www-data:www-data app/data/
+chown -R www-data:www-data app/runtime/
+chown -R www-data:www-data www/assets/
+chown -R www-data:www-data www/runtime/
+chmod -R g+rw app/data/
+chmod -R g+rw app/runtime/
+chmod -R g+rw www/assets/
+chmod -R g+rw www/runtime/
+app/yiic migrate --connectionID=$connectionID
+app/yiic authorizationhierarchy reset
+app/yiic authorizationhierarchy build
+app/yiic databaseviewgenerator --connectionID=$connectionID item
+app/yiic fixture --connectionID=$connectionID load
