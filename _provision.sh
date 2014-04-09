7f9f96fa5fccd29b6fd46889f050f34d23104261
Fredrik Wollsén <fredrik@neam.se>
2014-04-09 20:36:50 +0300

    Skipping non-working part of post-buildstep.sh

--- _provision.sh
+++ _provision.sh
@@ -3,5 +3,6 @@ set -x
 npm install
 bower install --allow-root
 php composer.phar install
+rm -r www/assets/*
 
 ./post-buildstep.sh
--- post-buildstep.sh
+++ post-buildstep.sh
@@ -1,7 +1,6 @@
 if [ "$connectionID" == "" ]; then
     connectionID=db
 fi
-rm -r www/assets/*
 chown -R www-data:www-data app/data/
 chown -R www-data:www-data app/runtime/
 chown -R www-data:www-data www/assets/
@@ -10,6 +9,8 @@ chmod -R g+rw app/data/
 chmod -R g+rw app/runtime/
 chmod -R g+rw www/assets/
 chmod -R g+rw www/runtime/
+# todo, fix envbootstrap before continuing
+exit 0
 app/yiic migrate --connectionID=$connectionID
 app/yiic authorizationhierarchy reset
 app/yiic authorizationhierarchy build
