e5bec9a1813346d77e902c67364c2a9140f105d6
Eric Nishio <eric.nishio@nordsoftware.com>
2014-04-10 10:31:34 +0300

    Auto-generate LESS imports with grunt-less-imports

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,10 +22,23 @@ module.exports = function(grunt) {
                 }
             }
         },
+        less_imports: {
+            options: {
+                inlineCSS: false
+            },
+            dist: {
+                dest: 'app/themes/gapminder/less/imports.less',
+                src: [
+                    'app/themes/gapminder/less/layouts/**/*.less',
+                    'app/themes/gapminder/less/controllers/**/*.less',
+                    'app/themes/gapminder/less/widgets/**/*.less'
+                ]
+            }
+        },
         watch: {
             styles: {
                 files: ['app/themes/gapminder/less/**/*.less'],
-                tasks: ['less', 'kss'],
+                tasks: ['less', 'less_imports', 'kss'],
                 options: {
                     spawn: false
                 }
@@ -35,6 +48,7 @@ module.exports = function(grunt) {
 
     // Load plugins
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-less-imports');
 
     // Define tasks
     grunt.registerTask('default', []);
