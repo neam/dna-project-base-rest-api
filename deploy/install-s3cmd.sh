8545f4ad9304799dd173c50a5b1e400328497db5
Fredrik Wollsén <fredrik@neam.se>
2014-04-16 10:04:05 +0300

    Provision script for s3cmd and supplying filepath to user-generated-data within s3

---
+++ deploy/install-s3cmd.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# install s3cmd
+wget -O- -q http://s3tools.org/repo/deb-all/stable/s3tools.key | sudo apt-key add -
+sudo wget -O/etc/apt/sources.list.d/s3tools.list http://s3tools.org/repo/deb-all/stable/s3tools.list
+sudo apt-get update && sudo apt-get install s3cmd
+
+exit 0
