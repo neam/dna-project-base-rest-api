39251c8d3117c6e10e1e60fa0fef2eb921042c25
Fredrik Wollsén <fredrik@neam.se>
2014-04-16 12:05:15 +0300

    Updated envbootstrap include for tests

--- tests/codeception/acceptance/_bootstrap.php
+++ tests/codeception/acceptance/_bootstrap.php
@@ -2,12 +2,8 @@
 // Here you can initialize variables that will for your tests
 $suite = "acceptance";
 
-$envbootstrap = dirname(__FILE__) . '/../../../../common/settings/envbootstrap.php';
-if (!is_readable($envbootstrap)) {
-    echo "Main envbootstrap file $envbootstrap not available.";
-    die(2);
-}
-require_once($envbootstrap);
+// envbootstrap
+require_once(dirname(__FILE__) . '/../envbootstrap-for-tests.php');
 
 \Codeception\Util\Autoload::registerSuffix('Steps', __DIR__.DIRECTORY_SEPARATOR.'_steps');
 \Codeception\Util\Autoload::registerSuffix('Page', __DIR__.DIRECTORY_SEPARATOR.'_pages');
