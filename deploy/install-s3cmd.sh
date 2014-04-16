9843baddea3e9e644760a8aed13ec12b0f77e01d
Fredrik Wollsén <fredrik@neam.se>
2014-04-16 10:38:47 +0300

    Adding s3cmd configuration on install

--- deploy/install-s3cmd.sh
+++ deploy/install-s3cmd.sh
@@ -5,4 +5,10 @@ wget -O- -q http://s3tools.org/repo/deb-all/stable/s3tools.key | sudo apt-key ad
 sudo wget -O/etc/apt/sources.list.d/s3tools.list http://s3tools.org/repo/deb-all/stable/s3tools.list
 sudo apt-get update && sudo apt-get install s3cmd
 
+if [ ! -f ~/.s3cfg ]; then
+    echo "[default]
+access_key = $USER_DATA_BACKUP_UPLOADERS_ACCESS_KEY
+secret_key = $USER_DATA_BACKUP_UPLOADERS_SECRET" > ~/.s3cfg
+fi
+
 exit 0
