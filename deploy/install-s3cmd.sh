3325d275b00505c4a58ff1e0787a4db217d204d3
Fredrik Wollsén <fredrik@neam.se>
2014-04-16 13:10:41 +0300

    S3cmd tweaks

--- deploy/install-s3cmd.sh
+++ deploy/install-s3cmd.sh
@@ -7,8 +7,4 @@ wget -O- -q http://s3tools.org/repo/deb-all/stable/s3tools.key | sudo apt-key ad
 sudo wget -O/etc/apt/sources.list.d/s3tools.list http://s3tools.org/repo/deb-all/stable/s3tools.list
 sudo apt-get update && sudo apt-get install s3cmd
 
-echo "[default]
-access_key = $USER_DATA_BACKUP_UPLOADERS_ACCESS_KEY
-secret_key = $USER_DATA_BACKUP_UPLOADERS_SECRET" > /tmp/.s3cfg
-
 exit 0
--- shell-scripts/upload-user-data-backup.sh
+++ shell-scripts/upload-user-data-backup.sh
@@ -2,6 +2,8 @@
 
 set -x
 
+script_path=`dirname $0`
+
 # Note: this script is tailored to be run on dokku deployments through sshcommand
 
 DATETIME=$(date +"%Y-%d-%m_%H%M%S")
@@ -11,9 +13,10 @@ FILEPATH=cms/$FILENAME
 # make php binaries available
 export PATH="/app/vendor/php/bin/:$PATH"
 
-app/yiic mysqldump --dumpFile=/tmp/$FILENAME
+$script_path/../app/yiic mysqldump --dumpFile=/tmp/$FILENAME
 
-s3cmd --access_key="$USER_DATA_BACKUP_UPLOADERS_ACCESS_KEY" --secret_key="$USER_DATA_BACKUP_UPLOADERS_SECRET" put /tmp/$FILENAME "$USER_GENERATED_DATA_S3_BUCKET/$FILEPATH"
+$script_path/configure-s3cmd.sh
+s3cmd --config=/tmp/.s3cfg --acl-private --guess-mime-type put /tmp/$FILENAME "$USER_GENERATED_DATA_S3_BUCKET/$FILEPATH"
 
 echo $FILEPATH > /app/db/user-generated-data.filepath
 
