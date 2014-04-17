57d12d09f5deb7a8e894655b91ad2e0859300c63
Fredrik Wollsén <fredrik@neam.se>
2014-04-17 11:51:35 +0300

    Moved loading of user-generated data into cms scripts and added consideration of user generated media

--- deploy/reset-db.sh
+++ deploy/reset-db.sh
@@ -2,14 +2,55 @@
 
 set -x;
 
+# fail on any error
+set -o errexit
+
 app/yiic databaseschema --connectionID=db dropAllTablesAndViews --verbose=0
 app/yiic databaseschema --connectionID=db loadSql --path=db/schema.sql --verbose=0
-app/yiic fixture --connectionID=db load
-app/yiic migrate --connectionID=db --interactive=0
-app/yiic databaseviewgenerator --connectionID=db item
 
 rm -rf app/data/p3media/*
 # todo: find a way to ensure that previously uploaded media can be restored from, possible similar to below but that works more than once
 #mkdir .trashed-p3media-data
 #mv app/data/p3media/* .trashed-p3media-data/
 
+if [ "$DATA" == "user-generated" ]; then
+
+    if [ ! -f db/user-generated-data.sql ]; then
+
+        echo "== Fetching the user-generated data associated with this commit =="
+
+        shell-scripts/configure-s3cmd.sh
+
+        export USER_GENERATED_DATA_S3_BUCKET="s3://user-data-backups"
+        export USER_GENERATED_DATA_FILEPATH=`cat db/user-generated-data.filepath`
+        export USER_GENERATED_DATA_S3_URL=$USER_GENERATED_DATA_S3_BUCKET/$USER_GENERATED_DATA_FILEPATH
+        s3cmd --config=/tmp/.s3cfg get "$USER_GENERATED_DATA_S3_URL" db/user-generated-data.sql
+
+        echo "User data dump downloaded from $USER_GENERATED_DATA_S3_URL to db/user-generated-data.sql"
+
+    fi
+
+    if [ ! -f db/user-generated-media/ ]; then
+
+        echo "== Fetching the user-generated media associated with this commit =="
+
+        export USER_GENERATED_MEDIA_S3_BUCKET="s3://user-data-backups"
+        export USER_GENERATED_MEDIA_FOLDERPATH=`cat db/user-generated-data.folderpath`
+        export USER_GENERATED_MEDIA_S3_URL=$USER_GENERATED_MEDIA_S3_BUCKET/$USER_GENERATED_MEDIA_FOLDERPATH
+        mkdir db/user-generated-media/
+        s3cmd --config=/tmp/.s3cfg get "$USER_GENERATED_MEDIA_S3_URL" db/user-generated-media/
+
+        echo "User media downloaded from $USER_GENERATED_DATA_S3_URL to db/user-generated-media/"
+
+    fi
+
+    echo "===== Load the user-generated data associated with this commit ===="
+
+    app/yiic fixture --connectionID=db load --dump=db/user-generated-data.sql
+    cp -r db/user-generated-p3media/* app/data/p3media/
+
+fi
+
+app/yiic fixture --connectionID=db load
+app/yiic migrate --connectionID=db --interactive=0
+app/yiic databaseviewgenerator --connectionID=db item
--- shell-scripts/upload-user-data-backup.sh
+++ shell-scripts/upload-user-data-backup.sh
@@ -6,20 +6,35 @@ script_path=`dirname $0`
 
 # Note: this script is tailored to be run on dokku deployments through sshcommand
 
-DATETIME=$(date +"%Y-%d-%m_%H%M%S")
-FILENAME=ENV-$ENV.$DATETIME.sql
-FILEPATH=cms/$FILENAME
+# configure s3cmd
+$script_path/configure-s3cmd.sh
 
 # make php binaries available
 export PATH="/app/vendor/php/bin/:$PATH"
 
-$script_path/../app/yiic mysqldump --dumpFile=/tmp/$FILENAME
+DATETIME=$(date +"%Y-%d-%m_%H%M%S")
+FOLDER=ENV-$ENV
 
-$script_path/configure-s3cmd.sh
+# dump and upload sql
+
+FILENAME=$DATETIME.sql
+FILEPATH=cms/$FOLDER/$FILENAME
+
+$script_path/../app/yiic mysqldump --dumpFile=/tmp/$FILENAME
 s3cmd --config=/tmp/.s3cfg --acl-private --guess-mime-type put /tmp/$FILENAME "$USER_GENERATED_DATA_S3_BUCKET/$FILEPATH"
 
 echo $FILEPATH > /app/db/user-generated-data.filepath
+echo "User data sql dump uploaded to $USER_GENERATED_DATA_S3_BUCKET/$FILEPATH"
+echo "Set the contents of 'db/user-generated-data.filepath' to '$FILEPATH' in order to use this upload"
 
-echo "User data backup uploaded to $USER_GENERATED_DATA_S3_BUCKET/$FILEPATH"
+# dump and upload user media
+
+FOLDERPATH=cms/$FOLDER/$DATETIME.user-generated-media/
+
+s3cmd --config=/tmp/.s3cfg --acl-private --guess-mime-type put $script_path/../app/data/p3media "$USER_GENERATED_DATA_S3_BUCKET/$FOLDERPATH"
+echo $FOLDERPATH > /app/db/user-generated-media.folderpath
+
+echo "User media uploaded to $USER_GENERATED_DATA_S3_BUCKET/$FOLDERPATH"
+echo "Set the contents of 'db/user-generated-media.folderpath' to '$FOLDERPATH' in order to use this upload"
 
 exit 0
