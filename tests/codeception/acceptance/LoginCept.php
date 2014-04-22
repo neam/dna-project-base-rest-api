f619201112146e8b27a6036f73d8905d77e61b3b
Fredrik Wollsén <fredrik@neam.se>
2014-04-22 12:12:27 +0300

    Removed tests not to be used in this release-branch

--- tests/codeception/acceptance/LoginCept.php
+++
@@ -1,5 +0,0 @@
-<?php
-$I = new WebGuy\MemberSteps($scenario);
-$I->wantTo('login and see result');
-$I->login('admin', 'admin');
-$I->see('Welcome to Gapminder', 'h4');
--- tests/codeception/acceptance/LoginOutCept.php
+++
@@ -1,6 +0,0 @@
-<?php
-$I = new WebGuy\MemberSteps($scenario);
-$I->wantTo('log out and see result');
-$I->login('admin', 'admin');
-$I->logout();
-$I->see('Login', HomePage::$loginLink);
