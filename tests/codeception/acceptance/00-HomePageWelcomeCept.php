ff99fcb933a254196803c65cfc243538c50b944f
Fredrik Wollsén <fredrik@neam.se>
2014-04-22 16:05:49 +0300

    Renamed the trivial acceptance test

--- tests/codeception/acceptance/00-HomePageWelcomeCept.php
+++ tests/codeception/acceptance/00-HomePageWelcomeCept.php
@@ -1,5 +1,5 @@
 <?php
 $I = new WebGuy\MemberSteps($scenario);
-$I->wantTo('check that running the simplest acceptance test works');
+$I->wantTo('check that the homepage shows a welcome message');
 $I->amOnPage(HomePage::$URL);
 $I->see('Welcome to Gapminder', 'h4');
