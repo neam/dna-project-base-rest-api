cf2f57db7e6720f8cf51c4e23372b629271462c8
Fredrik Wollsén <fredrik@neam.se>
2014-04-23 16:50:16 +0300

    Added comment about s3cmd version

--- deploy/install-s3cmd.sh
+++ deploy/install-s3cmd.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-# install s3cmd
+# install s3cmd 1.1.0-beta3 (manual install: http://sourceforge.net/projects/s3tools/files/s3cmd/1.1.0-beta3/s3cmd-1.1.0-beta3.zip/download)
 wget -O- -q http://s3tools.org/repo/deb-all/stable/s3tools.key | sudo apt-key add -
 sudo wget -O/etc/apt/sources.list.d/s3tools.list http://s3tools.org/repo/deb-all/stable/s3tools.list
 sudo apt-get update && sudo apt-get install -y -q s3cmd
