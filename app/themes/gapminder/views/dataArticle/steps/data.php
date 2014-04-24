70c34f07a4e6b5da74eaff16d1009b2324b3aadc
Eric Nishio <eric.nishio@nordsoftware.com>
2014-04-24 16:51:34 +0300

    Fix data article views

--- app/themes/gapminder/views/dataArticle/steps/data.php
+++ app/themes/gapminder/views/dataArticle/steps/data.php
@@ -8,7 +8,12 @@
 $relation = 'fileMedia';
 $attribute = 'file_media_id';
 $step = 'data';
-$mimeTypes = array('application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'text/csv');
+$mimeTypes = array(
+    'application/vnd.ms-excel',
+    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+    'text/csv',
+);
 
 $this->renderPartial('//p3Media/_select', compact('model', 'form', 'relation', 'attribute', 'step', 'mimeTypes'));
 ?>
