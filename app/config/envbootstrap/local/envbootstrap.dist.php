0c8490d6d6917696e5aa1f6c42f8ed0769c2286f
Christoffer Niska <christoffer.niska@nordsoftware.com>
2014-05-07 13:09:37 +0300

    Fix and refactor envbootstrap files

--- app/config/envbootstrap/local/envbootstrap.dist.php
+++ app/config/envbootstrap/local/envbootstrap.dist.php
@@ -8,10 +8,11 @@ define("ENV", 'local-foo');
 define("DEV", true);
 define("DEBUG_REDIRECTS", false);
 define("DEBUG_LOGS", false);
+define("CONFIG_ENVIRONMENT", 'development');
 
 // ==== Identity-related constants ====
 
-define("BRAND_SITENAME", 'Gapminder CMS Foo');
+define("BRAND_SITENAME", 'Gapminder CMS Foo DEV');
 define("BRAND_DOMAIN", 'gapminder.local');
 
 // ==== Defines infrastructure = all backing services, usernames, api:s, servers, ports etc depending on environment ====
@@ -24,31 +25,32 @@ define("SMTP_AUTH_USERNAME", '');
 define("SMTP_AUTH_PASSWORD", '');
 
 // Support setting main db constants based on DATABASE_URL environment variable
-define("DATABASE_URL", '');
-if (!is_null(DATABASE_URL)) {
+define("DATABASE_URL", null);
 
+if (DATABASE_URL === null) {
+    define("YII_DB_SCHEME", "mysql");
+    define("YII_DB_HOST", "127.0.0.1");
+    define("YII_DB_USER", "root");
+    define("YII_DB_PASSWORD", "root");
+    define("YII_DB_NAME", "gcms");
+} else {
     // get the environment variable and parse it:
     $url = parse_url(DATABASE_URL);
     define("YII_DB_SCHEME", $url['scheme']);
     define("YII_DB_HOST", $url['host']);
     define("YII_DB_PORT", $url['port']);
-    define("YII_DB_NAME", trim($url['path'], '/'));
     define("YII_DB_USER", $url['user']);
     define("YII_DB_PASSWORD", $url['pass']);
-
-} else {
-
-    define("YII_DB_SCHEME", 'mysql');
-    define("YII_DB_NAME", '');
-    define("YII_DB_USER", '');
-    define("YII_DB_PASSWORD", '');
-    define("YII_DB_HOST", '');
-
+    define("YII_DB_NAME", trim($url['path'], '/'));
 }
 
-define("TEST_DB_NAME", YII_DB_NAME . '_test');
-define("TEST_DB_USER", YII_DB_USER . '_test');
-define("TEST_DB_PASSWORD", YII_DB_USER . '_test');
+define("YII_GII_PASSWORD", "foo");
+
+// ==== Define test-related constants ====
+
+define("TEST_DB_NAME", 'gscms_test');
+define("TEST_DB_USER", 'gcms_test');
+define("TEST_DB_PASSWORD", 'gcms_test');
 define("TEST_DB_HOST", YII_DB_HOST);
 
 // ==== Define some dependent constants and/or settings ====
@@ -66,19 +68,15 @@ if (DEV) {
 
 class Identity
 {
-
     static public function brand()
     {
-
         $return = new \stdClass();
         $return->siteName = BRAND_SITENAME;
         $return->domain = BRAND_DOMAIN;
         $return->supportEmail = "info@" . $return->domain;
         $return->mailSentByMail = "noreply@" . $return->domain;
         $return->mailSentByName = $return->siteName;
-
     }
-
 }
 
 
