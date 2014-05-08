85f2b432cf8bb3dcbae7e4c6791dfe2026c1524c
Fredrik Wollsén <fredrik@neam.se>
2014-05-08 16:27:29 +0200

    CMS-736 Add checks for the required items to the acceptance tests #solution

--- tests/codeception/api/RetrieveGoRequiredItemsCept.php
+++ tests/codeception/api/RetrieveGoRequiredItemsCept.php
@@ -4,7 +4,46 @@
 $scenario->group('data:user-generated');
 $I = new ApiGuy($scenario);
 $I->wantTo('retrieve go-required items via the REST API');
+
 $I->sendGET('i18nCatalog/get', array('id' => 3));
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
+
+$I->sendGET('language/list');
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+
+$I->sendGET('i18nCatalog/poJson', array('id' => 4, 'lang' => 'en'));
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+
+$I->sendGET('i18nCatalog/poJson', array('id' => 4, 'lang' => 'fa'));
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+
+$I->sendGET('videoFile/poJson', array('id' => 2, 'lang' => 'en'));
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+
+$I->sendGET('videoFile/poJson', array('id' => 2, 'lang' => 'fa'));
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+
+$I->sendGET('snapshot/poJson', array('id' => 5, 'lang' => 'en'));
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+
+$I->sendGET('snapshot/poJson', array('id' => 5, 'lang' => 'fa'));
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+
+$I->sendGET('en/videoFile/subtitles', array('id' => 2, 'lang' => 'fa'));
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+
+$I->sendGET('fa/videoFile/subtitles', array('id' => 2, 'lang' => 'fa'));
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+
 */
+
