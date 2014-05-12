1b46f8dcee29cf31f25fa183d27853092e26cada
Eric Nishio <eric.nishio@nordsoftware.com>
2014-05-12 09:46:32 +0300

    Print full language name in translation tasks

--- app/themes/gapminder/views/DashboardTaskList/_translationTask.php
+++ app/themes/gapminder/views/DashboardTaskList/_translationTask.php
@@ -44,7 +44,7 @@
         <div class="task-progress">
             <div class="progress-info">
                 <span class="progress-title">
-                    <?php echo Html::encode($data['language']); // TODO: get language name dynamically ?>
+                    <?php echo Html::encode(Yii::app()->getLanguageNameByCode($data['language'])); // TODO: get language name dynamically ?>
                 </span>
                 <span class="progress-percentage">
                     <?php echo (int)$data['progress'] . '%'; ?>
