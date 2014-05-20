8c0dd172a9c9666c0ca72fa81176d9ae9c5f745a
Jimi Pirilä <jimi.pirila@nordsoftware.com>
2014-05-20 15:50:50 +0300

    Current progress on video uploading

--- tests/codeception/acceptance/VideoFile/01-CreateNewCept.php
+++ tests/codeception/acceptance/VideoFile/01-CreateNewCept.php
@@ -13,6 +13,17 @@ $I->createVideoFile(
         'info' => array(
             VideoFileEditPage::$titleField => 'Max video',
         ),
+        'files' => array(
+            VideoFileEditPage::$webmField => function () use ($I) {
+                $modalContext = '#item-form-webm-modal';
+                $I->click('Upload new', '.webm');
+                $I->waitForElementVisible($modalContext, 30);
+                $I->switchToIFrame('item-form-webm-upload-iframe');
+                $I->attachFile('input[name="files[]"]', 'big-buck-bunny_trailer.webm');
+                $I->see('big-buck-bunny_trailer.webm');
+                $I->click('.start .ui-button');
+            },
+        ),
     )
 );
 
@@ -22,4 +33,3 @@ $I->logout();
 
 $I->login('ola', 'test');
 $I->dontSeeVideoFile('Max video');
-
