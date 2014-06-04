08c1bd0435050240ab1a36872416dbcf0a3e5d71
Jimi Pirilä <jimi.pirila@nordsoftware.com>
2014-06-04 11:17:38 +0300

    Add subtitles to video-file CreateNewCept Restore mediaelement-container present assertion

--- tests/codeception/acceptance/VideoFile/01-CreateNewCept.php
+++ tests/codeception/acceptance/VideoFile/01-CreateNewCept.php
@@ -7,6 +7,25 @@ $I->wantTo('create a video and make sure that only I see it.');
 // Max logs in.
 $I->login('max', 'test');
 
+$subtitles = <<<EOD
+1
+00:00:03,399 --> 00:00:10,800
+A common misunderstanding is that if we save all the poor children: the world will become overpopulated.
+
+2
+00:00:10,800 --> 00:00:13,599
+This may sound logical, but it's wrong.
+
+3
+00:00:13,599 --> 00:00:16,199
+Its the other way around!
+
+4
+00:00:16,199 --> 00:00:22,000
+Saving the poor childrens lives is required to end population growth.
+EOD;
+
+
 // Max creates a new video file.
 $I->createVideoFile(
     array(
@@ -18,6 +37,9 @@ $I->createVideoFile(
                 $I->uploadWebmFile();
             },
         ),
+        'subtitles' => array(
+            VideoFileEditPage::$subtitlesField => $subtitles,
+        ),
     )
 );
 
@@ -25,8 +47,7 @@ $I->amOnPage(VideoFileBrowsePage::$URL);
 $I->seeVideoFile('Max video');
 $I->click('View', VideoFileBrowsePage::modelContext('Max video'));
 
-// TODO: find out why mediaelement borks when testing but not if tested manually
-//$I->seeElement(VideoFileViewPage::$videoContainer);
+$I->seeElement(VideoFileViewPage::$videoContainer);
 $I->dontSee(VideoFileViewPage::$noVideoMessage);
 $I->logout();
 
