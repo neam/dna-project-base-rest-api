02d5ed53308b9e78f9b02d45d2e075bdf7c4f84d
Jimi Pirilä <jimi.pirila@nordsoftware.com>
2014-06-05 10:31:36 +0300

    Change registration text to same as in AppActivationController so that test don't fail because of different yii-user configurations

--- app/themes/gapminder/views/user/registration-success.php
+++ app/themes/gapminder/views/user/registration-success.php
@@ -4,7 +4,7 @@
             <div class="brand-logo">
                 <h1><?php echo Html::renderLogoWithLink('Gapminder') ?></h1>
             </div>
-            <p><?php echo Yii::t('app', 'Thank you for registering! You will receive a confirmation email as soon as your application has been reviewed.'); ?></p>
+            <p><?php echo Yii::t('app', 'Thank you for your registration. You will receive a confirmation email as soon as your application has been reviewed.'); ?></p>
             <div class="page-actions">
                 <?php echo TbHtml::linkButton(
                     Yii::t('app', 'Back to Front Page'),
