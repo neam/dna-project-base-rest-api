aba91fd1845cc820d3095255377c31f6605df552
Jimi Pirilä <jimi.pirila@nordsoftware.com>
2014-06-05 10:50:32 +0300

    Add comments to warn developers that registration text is used by acceptance tests

--- app/themes/gapminder/views/user/registration-success.php
+++ app/themes/gapminder/views/user/registration-success.php
@@ -4,6 +4,7 @@
             <div class="brand-logo">
                 <h1><?php echo Html::renderLogoWithLink('Gapminder') ?></h1>
             </div>
+            <?php // NOTE! the following text ("Thank you for your registration.") is used by acceptance tests to test registration success. ?>
             <p><?php echo Yii::t('app', 'Thank you for your registration. You will receive a confirmation email as soon as your application has been reviewed.'); ?></p>
             <div class="page-actions">
                 <?php echo TbHtml::linkButton(
