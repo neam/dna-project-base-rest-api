3a0c171ef1accc1ef63f59aebab0a46a9867d97c
Jimi Pirilä <jimi.pirila@nordsoftware.com>
2014-06-06 11:10:11 +0300

    Move adding video-file subtitles from CreateNew to TranslateCept

--- tests/codeception/acceptance/VideoFile/01-CreateNewCept.php
+++ tests/codeception/acceptance/VideoFile/01-CreateNewCept.php
@@ -7,25 +7,6 @@ $I->wantTo('create a video and make sure that only I see it.');
 // Max logs in.
 $I->login('max', 'test');
 
-$subtitles = <<<EOD
-1
-00:00:03,399 --> 00:00:10,800
-A common misunderstanding is that if we save all the poor children: the world will become overpopulated.
-
-2
-00:00:10,800 --> 00:00:13,599
-This may sound logical, but it's wrong.
-
-3
-00:00:13,599 --> 00:00:16,199
-Its the other way around!
-
-4
-00:00:16,199 --> 00:00:22,000
-Saving the poor childrens lives is required to end population growth.
-EOD;
-
-
 // Max creates a new video file.
 $I->createVideoFile(
     array(
@@ -36,9 +17,6 @@ $I->createVideoFile(
             VideoFileEditPage::$webmField => function () use ($I) {
                 $I->uploadWebmFile();
             },
-        ),
-        'subtitles' => array(
-            VideoFileEditPage::$subtitlesField => $subtitles,
         ),
     )
 );
--- tests/codeception/acceptance/VideoFile/07-DoTranslateCept.php
+++ tests/codeception/acceptance/VideoFile/07-DoTranslateCept.php
@@ -34,6 +34,33 @@ $I->login('max', 'test');
 $I->amOnPage(VideoFileBrowsePage::$URL);
 $I->see('Max video');
 
+$subtitles = <<<EOD
+1
+00:00:03,399 --> 00:00:10,800
+A common misunderstanding is that if we save all the poor children: the world will become overpopulated.
+
+2
+00:00:10,800 --> 00:00:13,599
+This may sound logical, but it's wrong.
+
+3
+00:00:13,599 --> 00:00:16,199
+Its the other way around!
+
+4
+00:00:16,199 --> 00:00:22,000
+Saving the poor childrens lives is required to end population growth.
+EOD;
+
+$I->editVideoFile(
+    'Max video',
+    array(
+        'subtitles' => array(
+            VideoFileEditPage::$subtitlesField => $subtitles,
+        ),
+    )
+);
+
 $videoContext = VideoFileBrowsePage::modelContext('Max video');
 
 $I->click('Translators', $videoContext);
