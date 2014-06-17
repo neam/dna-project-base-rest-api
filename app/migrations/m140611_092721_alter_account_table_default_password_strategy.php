afe8180ea24d4890224a7adb9db17b6009a2edab
Eric Nishio <eric.nishio@nordsoftware.com>
2014-06-17 12:38:49 +0300

    Fix database migrations

--- app/migrations/m140611_092721_alter_account_table_default_password_strategy.php
+++ app/migrations/m140611_092721_alter_account_table_default_password_strategy.php
@@ -4,11 +4,11 @@ class m140611_092721_alter_account_table_default_password_strategy extends EDbMi
 {
     public function safeUp()
     {
-        $this->alterColumn('account', 'passwordStrategy', "string NOT NULL DEFAULT 'legacy'");
+        //$this->alterColumn('account', 'passwordStrategy', "string NOT NULL DEFAULT 'legacy'");
 	}
 
     public function safeDown()
     {
-        $this->alterColumn('account', 'passwordStrategy', 'string NULL DEFAULT NULL');
+        //$this->alterColumn('account', 'passwordStrategy', 'string NULL DEFAULT NULL');
     }
 }
