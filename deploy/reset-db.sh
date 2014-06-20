df40a1a2a8cdb5d4849e2fbf40f2331f9ab1f419
Fredrik Wollsén <fredrik@neam.se>
2014-06-20 16:54:00 +0300

    Using sudo in reset-db script to be able to run as ordinary user

--- deploy/reset-db.sh
+++ deploy/reset-db.sh
@@ -35,14 +35,14 @@ if [ "$DATA" == "user-generated" ]; then
     mysql -A --host=$DB_HOST --port=$DB_PORT --user=$DB_USER --password=$DB_PASSWORD $DB_NAME < db/migration-base/user-generated/data.sql
 
     # copy the downloaded data to the p3media folder
-    rm -rf app/data/p3media/*
+    sudo rm -rf app/data/p3media/*
     # todo: find a way to ensure that previously uploaded media can be restored from, possible similar to below but that works more than once
     #mkdir .trashed-p3media-data
     #mv app/data/p3media/* .trashed-p3media-data/
     cp -r db/migration-base/user-generated/media/* app/data/p3media/
 
     # make downloaded media directories owned and writable by the web server
-    chown -R nobody: app/data/p3media/
+    sudo chown -R nobody: app/data/p3media/
 
 fi
 
