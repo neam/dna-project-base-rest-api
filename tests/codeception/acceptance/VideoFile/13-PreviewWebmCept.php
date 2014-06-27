5f0e2e3d91b4baec45327d7b90644904c705d6f9
Eric Nishio <eric.nishio@nordsoftware.com>
2014-06-27 14:52:26 +0300

    Add acceptance test for previewing .webm videos

---
+++ tests/codeception/acceptance/VideoFile/13-PreviewWebmCept.php
@@ -0,0 +1,26 @@
+<?php
+
+$scenario->group('data:clean-db');
+$I = new WebGuy\MemberSteps($scenario);
+$I->wantTo('create a video, attach a .webm video, and make sure it is rendered on the preview page.');
+$I->login('max', 'test');
+$videoTitle = '.webm Video';
+$I->createVideoFile(
+    array(
+        'info' => array(
+            VideoFileEditPage::$titleField => $videoTitle,
+        ),
+        'files' => array(
+            VideoFileEditPage::$webmField => function () use ($I) {
+                $I->uploadWebmFile();
+            },
+        ),
+    )
+);
+$I->amOnPage(VideoFileBrowsePage::$URL);
+$I->seeVideoFile($videoTitle);
+$I->click('Edit', VideoFileBrowsePage::modelContext($videoTitle));
+$I->see($videoTitle, 'h1');
+$I->click('Preview');
+$I->seeInTitle('Preview');
+$I->seeElement('.mejs-video');
