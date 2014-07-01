56fc2aa9b95ddd37e1f44b2c15ef25621f1e51fd
Jimi Pirilä <jimi.pirila@nordsoftware.com>
2014-07-01 14:49:14 +0300

    Acceptance test for GroupPublisher

--- tests/codeception/acceptance/Permissions/10-GroupPublisherCept.php
+++ tests/codeception/acceptance/Permissions/10-GroupPublisherCept.php
@@ -7,3 +7,11 @@ $I->wantTo('perform actions and see result');
  * Can toggle Item visibility to PUBLIC in assigned group
  */
 $I->login('publisher', 'test');
+
+$I->amOnPage(SnapshotBrowsePage::$URL);
+$I->see('Sample Snapshot');
+$I->click(
+    SnapshotBrowsePage::$publishButtonText,
+    SnapshotBrowsePage::modelContext('Sample Snapshot')
+);
+$I->waitForText(SnapshotBrowsePage::$unPublishButtonText);
