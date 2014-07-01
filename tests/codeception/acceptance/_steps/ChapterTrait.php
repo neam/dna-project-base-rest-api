e53b30f121f9f8f26e001293614a5a09cabe4258
Jimi Pirilä <jimi.pirila@nordsoftware.com>
2014-07-01 15:11:42 +0300

    Move functionality from MemberSteps to traits

---
+++ tests/codeception/acceptance/_steps/ChapterTrait.php
@@ -0,0 +1,25 @@
+<?php
+
+trait ChapterTrait
+{
+
+    use ItemEditTrait;
+
+    function createChapter($stepAttributes)
+    {
+        $I = $this;
+        $I->amGoingTo('create a new chapter');
+        $I->amOnPage(ChapterBrowsePage::$URL);
+        $I->click(ChapterBrowsePage::$addButton);
+        $I->fillItemStepPages(ChapterEditPage::$steps, $stepAttributes);
+    }
+
+    function editChapter($title, $stepAttributes)
+    {
+        $I = $this;
+        $I->amGoingTo('edit a existing chapter');
+        $I->amOnPage(ChapterBrowsePage::$URL);
+        $I->click(ChapterBrowsePage::$editButtonText, ChapterBrowsePage::modelContext($title));
+        $I->fillItemStepPages(ChapterEditPage::$steps, $stepAttributes);
+    }
+}
