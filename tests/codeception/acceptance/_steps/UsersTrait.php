eac63a0f66575d9415afb0482c2e92a960e1be52
Jimi Pirilä <jimi.pirila@nordsoftware.com>
2014-07-02 14:29:38 +0300

    Comment out the element-change callback and replace it with static 1s wait until found out why error-class does not disappear

--- tests/codeception/acceptance/_steps/UsersTrait.php
+++ tests/codeception/acceptance/_steps/UsersTrait.php
@@ -160,18 +160,24 @@ trait UsersTrait
             ? $I->checkOption(RegistrationPage::$acceptTermsField)
             : $I->uncheckOption(RegistrationPage::$acceptTermsField);
 
+        /*
+        // TODO: find out why administrator registering stalls
         // Wait until errors are cleared (ajax validation)
         $I->waitForElementChange(
             RegistrationPage::$formId,
-            function (\WebDriverElement $element) {
+            function (\WebDriverElement $element) use ($I) {
                 try {
                     $element->findElement(WebDriverBy::cssSelector(RegistrationPage::$errorClass));
                 } catch (NoSuchElementException $e) {
                     return true;
                 }
+
+                return false;
             },
             30
         );
+        */
+        $I->wait(1);
 
         $I->click(RegistrationPage::$submitButton);
 
