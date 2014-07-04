44a25987cb5d1118e15fd069ec02ad34ba114a96
Fredrik Wollsén <fredrik@neam.se>
2014-07-04 13:20:30 +0300

    Generated, auto-formatted and run empty unit test for progress calculations

---
+++ tests/codeception/unit/ProgressCalculationsTest.php
@@ -0,0 +1,27 @@
+<?php
+
+class ProgressCalculationsTest extends \Codeception\TestCase\Test
+{
+    /**
+     * @var \CodeGuy
+     */
+    protected $guy;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    /**
+     * @group data:clean-db
+     */
+    public function testMe()
+    {
+
+    }
+
+}
