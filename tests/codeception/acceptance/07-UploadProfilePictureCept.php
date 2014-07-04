7d51f60b4a87a54b1f68d88a866fb93fec239d26
Fredrik Wollsén <fredrik@neam.se>
2014-07-04 11:51:01 +0300

    CMS-921 Uploading files dont work in acceptance tests #solution

--- tests/codeception/acceptance/07-UploadProfilePictureCept.php
+++ tests/codeception/acceptance/07-UploadProfilePictureCept.php
@@ -3,8 +3,7 @@
 $scenario->group('data:clean-db');
 
 $I = new WebGuy\MemberSteps($scenario);
-$I->wantTo('upload a profile picture (inactivated)');
-/*
+$I->wantTo('upload a profile picture');
 $I->login('max', 'test');
 
 $I->amOnPage(ProfilePage::$URL);
@@ -22,4 +21,3 @@ $I->waitForText('Save');
 $I->click('Save');
 $I->waitForText('Your account information has been updated.');
 $I->seeSelect2OptionIsSelected('#Profile_picture_media_id', 'phundament.png');
-*/
