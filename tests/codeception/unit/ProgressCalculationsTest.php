beb3de5741b8aa6cba9e4c88cb01d0bf04fe305d
Fredrik Wollsén <fredrik@neam.se>
2014-07-10 14:24:24 +0300

    Finished test testZeroProgressOnNewItem (which fails)

--- tests/codeception/unit/ProgressCalculationsTest.php
+++ tests/codeception/unit/ProgressCalculationsTest.php
@@ -7,8 +7,13 @@ class ProgressCalculationsTest extends \Codeception\TestCase\Test
      */
     protected $guy;
 
+    protected $_snapshot;
+
     protected function _before()
     {
+        // Create an empty Snapshot
+        $this->_snapshot = new Snapshot();
+
     }
 
     protected function _after()
@@ -19,8 +24,16 @@ class ProgressCalculationsTest extends \Codeception\TestCase\Test
     /**
      * @group data:clean-db
      */
-    public function testMe()
+    public function testZeroProgressOnNewItem()
     {
+
+        $scenarios = $this->_snapshot->qaStateBehavior()->scenarios;
+
+        codecept_debug("");
+        foreach ($scenarios as $scenario) {
+            codecept_debug("Running against scenario '$scenario'");
+            $this->assertEquals(0, $this->_snapshot->calculateValidationProgress($scenario));
+        }
 
     }
 
