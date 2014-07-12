9947379d6408a09e3be5a527a85e67024a1ac2ae
Fredrik Wollsén <fredrik@neam.se>
2014-07-12 03:35:14 +0200

    Preventing returnUrl (after login) to be captcha, ajax requests, image requests nor api requests

--- app/components/LoginReturnUrlTracker.php
+++ app/components/LoginReturnUrlTracker.php
@@ -22,6 +22,9 @@ class LoginReturnUrlTracker extends CApplicationComponent
         if ($action == "user/login") {
             return;
         }
+        if ($action == "user/registration/captcha") {
+            return;
+        }
         if ($action == "account/authenticate/logout") {
             return;
         }
@@ -34,6 +37,23 @@ class LoginReturnUrlTracker extends CApplicationComponent
         if ($action == "videoFile/subtitles") {
             return;
         }
+        if (substr(strrev($action), 0, 4) == strrev(".png")) {
+            return;
+        }
+        if (substr(strrev($action), 0, 4) == strrev(".jpg")) {
+            return;
+        }
+        if (substr(strrev($action), 0, 4) == strrev(".gif")) {
+            return;
+        }
+        if (substr($action, 0, 4) == "api/") {
+            return;
+        }
+        if (Yii::app()->getRequest()->isAjaxRequest) {
+            return;
+        }
+
+        // todo: add 404s (which is tricky because the 404-check is way later than this component's execution time)
 
         // Keep track of the most recently visited valid url
         Yii::app()->user->returnUrl = Yii::app()->request->url;
