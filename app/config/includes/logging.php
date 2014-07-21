ab2886dd710ea2b16a2fd157457aceb5ad5a1746
Fredrik Wollsén <fredrik@neam.se>
2014-07-21 18:47:30 +0300

    Enabled webroute logging when DEBUG_LOGS is true except for ajax requests

--- app/config/includes/logging.php
+++ app/config/includes/logging.php
@@ -26,10 +26,12 @@ if (DEBUG_LOGS) {
         $levels .= ", " . $_GET['debug_extra_log_levels'];
     }
 
+    $ajaxRequest = isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH']==='XMLHttpRequest';
+
     $config["components"]["log"]["routes"][] = array(
         'class' => 'CWebLogRoute',
         'levels' => $levels, //trace,
-        'enabled' => false,
+        'enabled' => !$ajaxRequest,
     );
 
     /* Persistent logs */
