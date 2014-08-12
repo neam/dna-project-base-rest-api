f397c61e8ca7325d6ad1b3d672d0fc3c168be8ef
Jimi Pirilä <jimi.pirila@nordsoftware.com>
2014-08-12 13:19:16 +0300

    Wait for welcome-text before asserting that the text is seen

--- tests/codeception/acceptance-init/01-HomePageWelcomeCept.php
+++ tests/codeception/acceptance-init/01-HomePageWelcomeCept.php
@@ -5,4 +5,5 @@ $scenario->group('data:user-generated');
 $I = new WebGuy\MemberSteps($scenario);
 $I->wantTo('check that the homepage shows a welcome message');
 $I->amOnPage(HomePage::$URL);
+$I->waitForText('Welcome to Gapminder', 20);
 $I->see('Welcome to Gapminder', 'h4');
--- tests/codeception/acceptance-init/02-LoginCept.php
+++ tests/codeception/acceptance-init/02-LoginCept.php
@@ -3,4 +3,5 @@ $scenario->group('data:clean-db');
 $I = new WebGuy\MemberSteps($scenario);
 $I->wantTo('login and see result');
 $I->login('admin', 'admin');
+$I->waitForText('Welcome to Gapminder', 20);
 $I->see('Welcome to Gapminder', 'h4');
