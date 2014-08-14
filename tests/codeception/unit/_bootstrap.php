ea2c34a618c427c0b1cdd8234c4823f17bde22b0
Fredrik Wollsén <fredrik@neam.se>
2014-08-14 09:32:52 +0300

    Restored unit-tests by moving up composer autoloader

--- tests/codeception/unit/_bootstrap.php
+++ tests/codeception/unit/_bootstrap.php
@@ -4,6 +4,9 @@ $_SERVER['SCRIPT_FILENAME'] = 'index-test.php';
 $_SERVER['SCRIPT_NAME'] =  '/index-test.php';
 $_SERVER['REQUEST_URI'] = 'index-test.php';
 
+// require composer autoloader
+require_once(dirname(__FILE__).'/../../../vendor/autoload.php');
+
 // change the following paths if necessary
 $yiit=dirname(__FILE__).'/../../../vendor/yiisoft/yii/framework/yiit.php';
 require_once($yiit);
@@ -11,9 +14,6 @@ $main=require(dirname(__FILE__).'/../../../app/config/main.php');
 $env=require(dirname(__FILE__)."/../../../app/config/environments/" . CONFIG_ENVIRONMENT . ".php");
 
 //require_once(dirname(__FILE__).'/WebTestCase.php');
-
-// require composer autoloader
-require_once(dirname(__FILE__).'/../../../vendor/autoload.php');
 
 $config = CMap::mergeArray($main, $env);
 $config['components']['db'] = $config['components']['dbTest'];
