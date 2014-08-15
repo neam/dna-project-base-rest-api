10bc6400cf7a603c88fd4a8b7d06b14214d89437
Fredrik Wollsén <fredrik@neam.se>
2014-08-15 12:28:52 +0300

    Verifying that test user can login

--- tests/codeception/acceptance-init/05-RegistrationCept.php
+++ tests/codeception/acceptance-init/05-RegistrationCept.php
@@ -8,3 +8,11 @@ $I->wantTo('verify that registering a user works');
 $datetime = date("YmdHis");
 
 $I->register('test-' . $datetime, 'test', 'test', 'dev+test-' . $datetime . '@gapminder.org', true);
+
+// TODO: Verify that login can't be made
+//$I->cantLogin();
+
+// TODO: Activate the account by clicking the activation link in the email
+
+// Verify that user can login
+$I->login('test-' . $datetime, 'test');
