95a3ff6a6353c309e20347ae6bbe726553a5cfd4
Fredrik Wollsén <fredrik@neam.se>
2014-08-15 14:52:53 +0300

    Made 05-RegistrationCept pass and run on user-generated again

--- tests/codeception/acceptance-init/05-RegistrationCept.php
+++ tests/codeception/acceptance-init/05-RegistrationCept.php
@@ -1,28 +1,30 @@
 <?php
 $scenario->group('data:clean-db,coverage:basic');
+$scenario->group('data:user-generated,coverage:basic');
 
 $I = new WebGuy\MemberSteps($scenario);
 $I->wantTo('verify that registering a user works');
 
 $datetime = date("YmdHis");
 
-$I->register('test_a-' . $datetime, 'test', 'test', 'dev+test_a-' . $datetime . '@gapminder.org', true);
+$I->register('testa' . $datetime, 'test', 'test', 'dev+testa' . $datetime . '@gapminder.org', true);
 
 // TODO: Verify that can't login - currently login is enabled immediately:
 //$I->cantLogin();
 
 // TODO: Activate the account by having an admin activate it manually
 
+// Verify that user can login and logout
+$I->login('testa' . $datetime, 'test');
+$I->logout();
 
-// Verify that user can login
-$I->login('test-' . $datetime, 'test');
-
-$I->register('test_b-' . $datetime, 'test', 'test', 'dev+test_b-' . $datetime . '@gapminder.org', true);
+$I->register('testb' . $datetime, 'test', 'test', 'dev+testb' . $datetime . '@gapminder.org', true);
 
 // TODO: Verify that login can't be made
 //$I->cantLogin();
 
 // TODO: Activate the account by clicking the activation link in the email
 
-// Verify that user can login
-$I->login('test_b-' . $datetime, 'test');
+// Verify that user can login and logout
+$I->login('testb' . $datetime, 'test');
+$I->logout();
