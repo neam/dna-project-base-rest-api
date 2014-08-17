f36f2402ceae4ccf33c9b26787bb9138b3f72a0d
Fredrik Wollsén <fredrik@neam.se>
2014-08-17 16:07:04 +0300

    Running unit tests against a test database (since running it ordinarily refuses to work)

--- deploy/dokku-run-tests.sh
+++ deploy/dokku-run-tests.sh
@@ -46,19 +46,37 @@ vendor/bin/codecept build
 # set the codeception test group arguments depending on DATA and COVERAGE
 source _set-codeception-group-args.sh
 
-# run unit tests
-../app/yiic mysqldump --connectionID=db --dumpPath=tests/codeception/_data/
-vendor/bin/codecept run unit $CODECEPTION_ARGS --fail-fast
+# run unit tests (against a test database)
+
+    # make sure the db_test database exists and is empty
+    export DB_NAME=db_test
+    echo "DROP DATABASE IF EXISTS $DB_NAME; CREATE DATABASE $DB_NAME;" | mysql -h$DB_HOST -P$DB_PORT -u$DB_USER --password=$DB_PASSWORD
+
+    # generate local test config
+    export CMS_HOST=localhost:5000
+    ./generate-local-codeception-config.sh
+    vendor/bin/codecept build
+
+    # reset the test database to a clean db satet
+    export CONFIG_ENVIRONMENT=test
+    export DATA=clean-db
+    connectionID=dbTest ../shell-scripts/reset-db.sh
+
+    # run the actual unit tests
+    ../app/yiic mysqldump --connectionID=dbTest --dumpPath=tests/codeception/_data/
+    vendor/bin/codecept run unit $CODECEPTION_ARGS --fail-fast
 
 # run acceptance tests
-export env=cms-saucelabs-chrome-win8
-#export env=cms-saucelabs-firefox-win7
-#export env=cms-saucelabs-chrome-osx-108
-vendor/bin/codecept run acceptance-init --env=$env $CODECEPTION_GROUP_ARGS --debug --fail-fast
-#mysqldump --user="$DB_USER" --password="$DB_PASSWORD" --host="$DB_HOST" --port="$DB_PORT" --no-create-db db > codeception/_data/dump.sql
-vendor/bin/codecept run acceptance --env=$env $CODECEPTION_GROUP_ARGS --debug --fail-fast
+
+    export env=cms-saucelabs-chrome-win8
+    #export env=cms-saucelabs-firefox-win7
+    #export env=cms-saucelabs-chrome-osx-108
+    vendor/bin/codecept run acceptance-init --env=$env $CODECEPTION_GROUP_ARGS --debug --fail-fast
+    #mysqldump --user="$DB_USER" --password="$DB_PASSWORD" --host="$DB_HOST" --port="$DB_PORT" --no-create-db db > codeception/_data/dump.sql
+    vendor/bin/codecept run acceptance --env=$env $CODECEPTION_GROUP_ARGS --debug --fail-fast
 
 # run api tests
-vendor/bin/codecept run api -g data:$DATA --debug --fail-fast
+
+    vendor/bin/codecept run api -g data:$DATA --debug --fail-fast
 
 exit 0
