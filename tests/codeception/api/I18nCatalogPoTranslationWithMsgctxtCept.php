ee8d1815c20ee1c29476280d229ad5dc4dbc761d
Christoffer Lindqvist <christoffer.lindqvist@nordsoftware.com>
2014-08-20 15:31:37 +0300

    Add translations migration and api test that checks the translated strings

---
+++ tests/codeception/api/I18nCatalogPoTranslationWithMsgctxtCept.php
@@ -0,0 +1,10 @@
+<?php
+$scenario->group('data:user-generated,coverage:basic');
+$I = new ApiGuy($scenario);
+$I->wantTo('check .po file translations containing msgctxt for i18nCatalog items retrieved via the REST API');
+
+$I->sendGET('i18nCatalog/poJson', array('id' => 4, 'lang' => 'pt_br'));
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+$I->seeResponseContains('"metal\u0004Chain":[null,"Corrente"]');
+$I->seeResponseContains('"restaurant\u0004Chain":[null,"Cadeia"]');
