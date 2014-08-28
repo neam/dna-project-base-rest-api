3356674622961a985a27b8bc15d508f2c26b4624
Jimi Pirilä <jimi.pirila@nordsoftware.com>
2014-08-28 11:33:16 +0300

    View-action for groups

--- app/controllers/GroupController.php
+++ app/controllers/GroupController.php
@@ -20,8 +20,14 @@ class GroupController extends Controller
             array(
                 'allow',
                 'actions' => array(
-                    'index',
                     'view',
+                ),
+                'users' => array('*'),
+            ),
+            array(
+                'allow',
+                'actions' => array(
+                    'index',
                     'create',
                     'update',
                     'editableSaver',
@@ -61,6 +67,7 @@ class GroupController extends Controller
     public function actionView($id)
     {
         $model = $this->loadModel($id);
+        // todo: create the view
         $this->render('view', array('model' => $model,));
     }
 
