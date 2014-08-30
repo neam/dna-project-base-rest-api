d86d5037b25084e4a206a2befb9e3d204801097e
Fredrik Wollsén <fredrik@neam.se>
2014-08-30 19:25:45 +0200

    CMS-1061 The db/current-schema.sql needs to be arranged to be correct and committed #solution

--- shell-scripts/update-current-schema-dump.sh
+++ shell-scripts/update-current-schema-dump.sh
@@ -5,4 +5,8 @@ cd $script_path/../
 
 app/yiic mysqldump --dumpPath=db --dumpFile=current-schema.sql --data=false
 
+# perform some clean-up on the dump file so that it needs to be committed less often
+sed -i '/-- Dump completed on/d' db/current-schema.sql
+sed -i 's/AUTO_INCREMENT=[0-9]*\b/\/\*AUTO_INCREMENT omitted\*\//' db/current-schema.sql
+
 exit 0
