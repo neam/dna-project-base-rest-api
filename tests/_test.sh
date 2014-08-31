31e8b0e60fbb03a7a2f5ff682876e13d6d6ac693
Fredrik Wollsén <fredrik@neam.se>
2014-08-31 10:51:42 +0200

    Updated _test to run all tests

--- tests/_test.sh
+++ tests/_test.sh
@@ -9,7 +9,7 @@ script_path=`dirname $0`
 
 export COVERAGE=full
 
-export CMS_HOST=localhost:11111
+export CMS_HOST=localhost:12121/friends
 $script_path/generate-local-codeception-config.sh
 
 cd $script_path
@@ -18,18 +18,23 @@ export DATA=clean-db
 source _set-codeception-group-args.sh
 connectionID=dbTest $script_path/../shell-scripts/reset-db.sh
 
-vendor/bin/codecept run unit $CODECEPTION_GROUP_ARGS --debug
+vendor/bin/codecept run unit $CODECEPTION_GROUP_ARGS --debug --fail-fast
 #vendor/bin/codecept run functional -g data:clean-db --debug
 touch testing
-vendor/bin/codecept run acceptance-init --env=cms-local-chrome $CODECEPTION_GROUP_ARGS --debug
-vendor/bin/codecept run acceptance --env=cms-local-chrome $CODECEPTION_GROUP_ARGS --debug
-vendor/bin/codecept run api $CODECEPTION_GROUP_ARGS --debug
+vendor/bin/codecept run acceptance-init --env=cms-local-chrome $CODECEPTION_GROUP_ARGS --debug --fail-fast
+vendor/bin/codecept run acceptance --env=cms-local-chrome $CODECEPTION_GROUP_ARGS --debug --fail-fast
+vendor/bin/codecept run api $CODECEPTION_GROUP_ARGS --debug --fail-fast
 rm testing
 
 export DATA=user-generated
 source _set-codeception-group-args.sh
 connectionID=dbTest $script_path/../shell-scripts/reset-db.sh
 
+vendor/bin/codecept run unit $CODECEPTION_GROUP_ARGS --debug --fail-fast
+#vendor/bin/codecept run functional -g data:clean-db --debug
+
 touch testing
-vendor/bin/codecept run api $CODECEPTION_GROUP_ARGS --debug
+vendor/bin/codecept run acceptance-init --env=cms-local-chrome $CODECEPTION_GROUP_ARGS --debug --fail-fast
+vendor/bin/codecept run acceptance --env=cms-local-chrome $CODECEPTION_GROUP_ARGS --debug --fail-fast
+vendor/bin/codecept run api $CODECEPTION_GROUP_ARGS --debug --fail-fast
 rm testing
