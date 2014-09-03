064781b947485e5bbe61c92ffc87fa137b4741db
Fredrik Wollsén <fredrik@neam.se>
2014-09-03 12:39:41 +0300

    Added selector to My Tasks clicks so that it doesn't try to click the one in the menu

--- tests/codeception/acceptance/_steps/DashboardTrait.php
+++ tests/codeception/acceptance/_steps/DashboardTrait.php
@@ -5,11 +5,12 @@ trait DashboardTrait
 
     function gotoMyTasksPage()
     {
+        /** @var \WebGuy\MemberSteps $I */
         $I = $this;
         $I->amOnPage(HomePage::$URL);
-        $I->waitForText('My Tasks', 20);
-        $I->click('My Tasks');
+        $I->waitForText('My Tasks', 20, 'div.content');
+        $I->click('My Tasks', 'div.content');
         $I->waitForText('Started Tasks', 20);
     }
 
-} 
+}
