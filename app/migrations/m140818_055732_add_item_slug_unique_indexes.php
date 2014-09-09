d8cca2d9fbc2e013d3aaae3eb5566e4cfa959343
Jimi Pirilä <jimi.pirila@nordsoftware.com>
2014-09-09 12:53:46 +0300

    Hardcode language-list to the state it was when migrations was created

--- app/migrations/m140818_055732_add_item_slug_unique_indexes.php
+++ app/migrations/m140818_055732_add_item_slug_unique_indexes.php
@@ -7,10 +7,60 @@ class m140818_055732_add_item_slug_unique_indexes extends EDbMigration
         'tool',
     );
 
+    private function getCodes()
+    {
+        return array(
+            'en',
+            'ar',
+            'bg',
+            'ca',
+            'cs',
+            'da',
+            'de',
+            'en_gb',
+            'en_us',
+            'el',
+            'es',
+            'fa',
+            'fi',
+            'fil',
+            'fr',
+            'hi',
+            'hr',
+            'hu',
+            'id',
+            'it',
+            'iw',
+            'ja',
+            'ko',
+            'lt',
+            'lv',
+            'nl',
+            'no',
+            'pl',
+            'pt',
+            'pt_br',
+            'pt_pt',
+            'ro',
+            'ru',
+            'sk',
+            'sl',
+            'sr',
+            'sv',
+            'th',
+            'tr',
+            'uk',
+            'vi',
+            'zh',
+            'zh_cn',
+            'zh_tw',
+        );
+    }
+
     public function up()
     {
         foreach ($this->tables as $table) {
-            foreach (LanguageHelper::getCodes() as $language) {
+            foreach (self::getCodes() as $language) {
                 $this->createIndex("slug_{$language}_UNIQUE", $table, "slug_{$language}", true);
             }
         }
@@ -19,7 +69,7 @@ class m140818_055732_add_item_slug_unique_indexes extends EDbMigration
     public function down()
     {
         foreach ($this->tables as $table) {
-            foreach (LanguageHelper::getCodes() as $language) {
+            foreach (self::getCodes() as $language) {
                 $this->dropIndex("slug_{$language}_UNIQUE", $table);
             }
         }
