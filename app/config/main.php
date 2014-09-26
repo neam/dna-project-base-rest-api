f430f831a92ed71e1aa05113d76b4ffdbe6be7ff
Fredrik Wollsén <fredrik@neam.se>
2014-09-26 15:31:16 +0200

    Tweaks for a working api/videoFile request

--- app/config/main.php
+++ app/config/main.php
@@ -15,6 +15,12 @@ $mainConfig = array(
         'vendor' => $applicationDirectory . '/../vendor',
         'dna' => $projectRoot . '/dna',
         'app' => 'application',
+        // fix hard-coded aliases
+        'ext.wrest' => 'vendor.weavora.wrest',
+        'ext.wrest.WRestController' => 'vendor.weavora.wrest.WRestController',
+        'ext.wrest.WHttpRequest' => 'vendor.weavora.wrest.WHttpRequest',
+        'ext.wrest.WRestResponse' => 'vendor.weavora.wrest.WRestResponse',
+        'ext.wrest.JsonResponse' => 'vendor.weavora.wrest.JsonResponse',
     ),
     // autoloading model and component classes
     'import' => array(
@@ -66,5 +72,8 @@ require($projectRoot . '/dna/dna-api-revisions/' . YII_DNA_REVISION . '/include.
 // Extensions' includes
 include($applicationDirectory . '/../vendor/neam/yii-dna-debug-modes-and-error-handling/config/error-handling.php');
 include($applicationDirectory . '/../vendor/neam/yii-dna-debug-modes-and-error-handling/config/debug-modes.php');
+
+// Uncomment to easily see the active merged configuration
+//echo "<pre>";print_r($config);echo "</pre>";die();
 
 return $config;
