8332288cf9ee43614eefce6690cf3f39c66e0877
Fredrik Wollsén <fredrik@neam.se>
2014-10-01 15:57:29 +0300

    Restored default yii log route config

--- app/config/main.php
+++ app/config/main.php
@@ -61,6 +61,15 @@ $mainConfig = array(
 
             ),
         ),
+        'log' => array(
+            'class' => 'CLogRouter',
+            'routes' => array(
+                array(
+                    'class' => 'CFileLogRoute',
+                    'levels' => 'error, warning',
+                ),
+            ),
+        ),
     ),
 );
 
