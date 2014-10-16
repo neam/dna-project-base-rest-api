a7d4410b69e37004bca1080dbe96e49b99b5409d
Christoffer Lindqvist <christoffer.lindqvist@nordsoftware.com>
2014-10-16 10:40:11 +0300

    Enable endpoint api/v1/language

--- app/controllers/BaseLanguageController.php
+++ app/controllers/BaseLanguageController.php
@@ -1,7 +1,13 @@
 <?php
 
+/**
+ * Language resource controller.
+ */
 class BaseLanguageController extends AppRestController
 {
+    /**
+     * Lists all language resources.
+     */
     public function actionList()
     {
         $this->sendResponse(200, LanguageHelper::getLanguageListWithDirection());
---
+++ app/modules/v1/controllers/LanguageController.php
@@ -0,0 +1,5 @@
+<?php
+
+class LanguageController extends BaseLanguageController
+{
+} 
