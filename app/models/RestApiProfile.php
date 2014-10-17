a70c1764b3d0ab62ec74c6f76e75c877189a9969
Christoffer Lindqvist <christoffer.lindqvist@nordsoftware.com>
2014-10-17 18:52:55 +0300

    Add hack for fixing sir trevor url encoding issue

--- app/models/RestApiProfile.php
+++ app/models/RestApiProfile.php
@@ -61,8 +61,8 @@ class RestApiProfile extends Profile
         $response->language1 = $this->language1;
         $response->language2 = $this->language2;
         $response->language3 = $this->language3;
-        $response->about_me = json_decode($this->about_me);
-        $response->my_links = json_decode($this->my_links);
+        $response->about_me = json_decode(str_replace('\\-', '-', $this->about_me)); // hack for fixing sir trevor urls where every "-" is a "\\-".
+        $response->my_links = json_decode(str_replace('\\-', '-', $this->my_links)); // hack for fixing sir trevor urls where every "-" is a "\\-".
 
         $response->contributions = array();
         foreach ($this->contributions as $contribution) {
