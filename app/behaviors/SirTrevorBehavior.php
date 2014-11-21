e661244d8db5f64ea8f40f2fc9b0c9fe548f48f5
Christoffer Lindqvist <kryysler@googlemail.com>
2014-11-21 16:34:45 +0200

    Add all item attributes to the response temporarily before we know which ones are needed

--- app/behaviors/SirTrevorBehavior.php
+++ app/behaviors/SirTrevorBehavior.php
@@ -27,11 +27,13 @@ class SirTrevorBehavior extends CActiveRecordBehavior
                     // If the node is restricted, just continue to the next one.
                     continue;
                 }
+                // todo: fix this once we know what data to include (i.e. when defined in apiary).
                 $block->data->attributes = array(
                     'id' => $item->id,
                     'title' => isset($item->title) ? $item->title : (isset($item->heading) ? $item->heading : null),
                     'about' => isset($item->about) ? $item->about : null,
                 );
+                $block->data->non_apiary_defined_attributes = $item->attributes;
             }
         }
         return $blocks;
