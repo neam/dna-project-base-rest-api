ee25e9ef338214f7e207c714d00510493c79270f
Christoffer Lindqvist <christoffer.lindqvist@nordsoftware.com>
2015-01-23 14:45:39 +0200

    Format profile response data

--- app/models/RestApiProfile.php
+++ app/models/RestApiProfile.php
@@ -56,46 +56,109 @@ class RestApiProfile extends Profile
      */
     public function getAllAttributes()
     {
-        $data = array(
+        return array(
             'first_name' => $this->first_name,
             'last_name' => $this->last_name,
+            'email' => (bool)$this->may_contact ? $this->account->email : null,
+            'social_links' => $this->getSocialLinkData(),
+            'may_contact' => (bool)$this->may_contact,
+            'professional_title' => json_decode($this->professional_title),
+            'lives_in' => $this->lives_in,
+            'language1' => $this->language1,
+            'language2' => $this->language2,
+            'language3' => $this->language3,
+            // hack for fixing sir trevor urls where every "-" is a "\\-". (https://github.com/madebymany/sir-trevor-js/issues/248)
+            'about_me' => json_decode(str_replace('\\\-', '-', $this->about_me)),
+            'my_links' => json_decode(str_replace('\\\-', '-', $this->my_links)),
+            'contributions' => $this->getContributionData(),
+            'profile_picture' => $this->getPictureUrl(),
+            'groups' => $this->getGroupData(),
         );
-        if ((bool)$this->may_contact) {
-            $data['email'] = $this->account->email;
-        }
-        $data['social_links'] = array();
+    }
+
+    /**
+     * Gets the social link data to include in response.
+     *
+     * Format:
+     *
+     * array(
+     *     array(
+     *         'name' => 'LinkedIn',
+     *         'url' => 'http://linkedin.com/profile'
+     *     ),
+     *     ...
+     * )
+     *
+     * @return array the data.
+     */
+    protected function getSocialLinkData()
+    {
+        $data = array();
         foreach ($this->socialLinks as $socialLink) {
-            $data['social_links'][] = array(
+            $data[] = array(
                 'name' => MetaData::socialLinkRefToLabel($socialLink->ref),
                 'url' => $socialLink->url,
             );
         }
-        $data['may_contact'] = (bool)$this->may_contact;
+        return $data;
+    }
 
-        $data['professional_title'] = json_decode($this->professional_title);
-        $data['lives_in'] = $this->lives_in;
-        $data['language1'] = $this->language1;
-        $data['language2'] = $this->language2;
-        $data['language3'] = $this->language3;
-        // hack for fixing sir trevor urls where every "-" is a "\\-". (https://github.com/madebymany/sir-trevor-js/issues/248)
-        $data['about_me'] = json_decode(str_replace('\\\-', '-', $this->about_me));
-        $data['my_links'] = json_decode(str_replace('\\\-', '-', $this->my_links));
-
-        $data['contributions'] = array();
+    /**
+     * Gets the contribution data to include in the response.
+     *
+     * Format:
+     *
+     * array(
+     *     array(
+     *         'label' => 'I did something',
+     *         'url' => 'http://some.url.com',
+     *         'datetime' => '2015-01-01 00:00:00',
+     *     ),
+     *     ...
+     * )
+     *
+     *
+     * @return array the data.
+     */
+    protected function getContributionData()
+    {
+        $data = array();
         foreach ($this->contributions as $contribution) {
-            $data['contributions'][] = array(
+            $data[] = array(
                 'label' => $contribution->label,
                 'url' => $contribution->url,
                 'datetime' => $contribution->datetime,
             );
         }
-        $data['profile_picture'] = $this->getPictureUrl();
+        return $data;
+    }
 
+    /**
+     * Gets the group data to include in the response.
+     *
+     * Format:
+     *
+     * array(
+     *     array(
+     *         'id' => 1,
+     *         'name' => 'Translators',
+     *         'member_label' => 'Member',
+     *         'roles' => array(
+     *             'GroupTranslator'
+     *         )
+     *     ),
+     *     ...
+     * )
+     *
+     * @return array the data.
+     */
+    protected function getGroupData()
+    {
         $groups = array();
         foreach ($this->account->groupHasAccounts as $gha) {
             if (!isset($groups[$gha->group->id])) {
                 $groups[$gha->group->id] = array(
-                    'id' => $gha->group->id,
+                    'id' => (int)$gha->group->id,
                     'name' => $gha->group->title,
                     'member_label' => 'Member', // todo: this is hard-coded for now (ask Fredrik & ChrisL for solution)
                     'roles' => array()
@@ -103,8 +166,6 @@ class RestApiProfile extends Profile
             }
             $groups[$gha->group->id]['roles'][] = $gha->role->title;
         }
-        $data['groups'] = array_values($groups);
-
-        return $data;
+        return array_values($groups);
     }
 }
