07de5fc6e08dc6ff94cb3f6c8e3311a8bcba9490
Christoffer Lindqvist <christoffer.lindqvist@nordsoftware.com>
2015-01-23 14:14:12 +0200

    Format profile response data

--- app/models/RestApiProfile.php
+++ app/models/RestApiProfile.php
@@ -3,6 +3,21 @@
 /**
  * Profile resource model.
  *
+ * @property string $first_name
+ * @property string $last_name
+ * @property int $may_contact
+ * @property string $professional_title
+ * @property string $lives_in
+ * @property string $language1
+ * @property string $language2
+ * @property string $language3
+ * @property string $about_me
+ * @property string $my_links
+ *
+ * @property Account $account
+ * @property SocialLink[] $socialLinks
+ * @property Contribution[] $contributions
+ *
  * Properties made available through the RestrictedAccessBehavior class:
  * @property boolean $enableRestriction
  */
@@ -41,39 +56,40 @@ class RestApiProfile extends Profile
      */
     public function getAllAttributes()
     {
-        $response = new stdClass();
-
-        $response->first_name = $this->first_name;
-        $response->last_name = $this->last_name;
+        $data = array(
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
+        );
         if ((bool)$this->may_contact) {
-            $response->email = $this->account->email;
+            $data['email'] = $this->account->email;
         }
-        $response->social_links = array();
+        $data['social_links'] = array();
         foreach ($this->socialLinks as $socialLink) {
-            $response->social_links[] = array(
+            $data['social_links'][] = array(
                 'name' => MetaData::socialLinkRefToLabel($socialLink->ref),
                 'url' => $socialLink->url,
             );
         }
-        $response->may_contact = (bool)$this->may_contact;
-        $response->professional_title = json_decode($this->professional_title);
-        $response->lives_in = $this->lives_in;
-        $response->language1 = $this->language1;
-        $response->language2 = $this->language2;
-        $response->language3 = $this->language3;
-        // hack for fixing sir trevor urls where every "-" is a "\\-". (https://github.com/madebymany/sir-trevor-js/issues/248)
-        $response->about_me = json_decode(str_replace('\\\-', '-', $this->about_me));
-        $response->my_links = json_decode(str_replace('\\\-', '-', $this->my_links));
+        $data['may_contact'] = (bool)$this->may_contact;
 
-        $response->contributions = array();
+        $data['professional_title'] = json_decode($this->professional_title);
+        $data['lives_in'] = $this->lives_in;
+        $data['language1'] = $this->language1;
+        $data['language2'] = $this->language2;
+        $data['language3'] = $this->language3;
+        // hack for fixing sir trevor urls where every "-" is a "\\-". (https://github.com/madebymany/sir-trevor-js/issues/248)
+        $data['about_me'] = json_decode(str_replace('\\\-', '-', $this->about_me));
+        $data['my_links'] = json_decode(str_replace('\\\-', '-', $this->my_links));
+
+        $data['contributions'] = array();
         foreach ($this->contributions as $contribution) {
-            $response->contributions[] = array(
+            $data['contributions'][] = array(
                 'label' => $contribution->label,
                 'url' => $contribution->url,
                 'datetime' => $contribution->datetime,
             );
         }
-        $response->profile_picture = $this->getPictureUrl();
+        $data['profile_picture'] = $this->getPictureUrl();
 
         $groups = array();
         foreach ($this->account->groupHasAccounts as $gha) {
@@ -81,13 +97,13 @@ class RestApiProfile extends Profile
                 $groups[$gha->group->id] = array(
                     'id' => $gha->group->id,
                     'name' => $gha->group->title,
-                    'member_label' => 'Member' // todo: this is hard-coded for now (ask Fredrik & ChrisL for solution)
+                    'member_label' => 'Member', // todo: this is hard-coded for now (ask Fredrik & ChrisL for solution)
+                    'roles' => array() // todo: add roles
                 );
             }
         }
-        $response->groups = array_values($groups);
+        $data['groups'] = array_values($groups);
 
-        return $response;
+        return $data;
     }
-
 }
