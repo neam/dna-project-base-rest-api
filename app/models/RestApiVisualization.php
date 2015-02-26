dac31b62d8095507143bfcf45a09cc78b2f43b83
Christoffer Lindqvist <christoffer.lindqvist@nordsoftware.com>
2015-02-26 16:43:15 +0200

    Add support for visualization sir trevor blocks

---
+++ app/models/RestApiVisualization.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * Properties available through the `I18nAttributeMessagesBehavior`:
+ * @property $title
+ */
+class RestApiVisualization extends Visualization implements SirTrevorBlock
+{
+    /**
+     * @inheritdoc
+     */
+    public static function model($className = __CLASS__)
+    {
+        return parent::model($className);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return array_merge(
+            parent::behaviors(),
+            array(
+                'rest-model-behavior' => array(
+                    'class' => 'WRestModelBehavior',
+                ),
+            )
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getCompositionItemType()
+    {
+        return 'visualization';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getCompositionAttributes()
+    {
+        return array(
+            'state' => json_decode($this->state, true),
+            'title' => $this->title,
+            'tool' => $this->getToolCompositionAttributes(),
+        );
+    }
+
+    /**
+     * Returns the `tool` relations composition data for this resource.
+     *
+     * @return array|null the data array or null if no tool is found.
+     */
+    protected function getToolCompositionAttributes()
+    {
+        if ($this->tool !== null) {
+            return array(
+                'ref' => $this->tool->ref,
+                'title' => $this->tool->title,
+                'slug' => $this->tool->slug,
+                'about' => $this->tool->about,
+                // i18nCatalog
+            );
+        }
+        return null;
+    }
+}
