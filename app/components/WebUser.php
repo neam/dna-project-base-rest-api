21cbf13fbe5c2efaa80326e30a347499595e0b74
Christoffer Lindqvist <christoffer.lindqvist@nordsoftware.com>
2015-03-12 10:32:50 +0200

    Allow requests to public resources even if provided OAuth access token is invalid

--- app/components/WebUser.php
+++ app/components/WebUser.php
@@ -14,29 +14,4 @@ class WebUser extends \OAuth2Yii\Component\WebUser
      * @var string the user model name.
      */
     public $modelClass = 'Account';
-
-    /**
-     * Treat the user as logged in user if a valid OAuth2 token is supplied.
-     */
-    public function init()
-    {
-        if ($this->getIsOAuth2Request()) {
-            /** @var $oauth2 \OAuth2yii\Component\ServerComponent */
-            $oauth2 = Yii::app()->getComponent($this->oauth2);
-            if ($oauth2 === null) {
-                throw new \CException("Invalid OAuth2Yii server component '{$this->oauth2}'");
-            }
-            if (($id = $oauth2->getUserId()) !== null) {
-                $this->_isOAuth2User = true;
-                $this->changeIdentity($id, 'oauth2user', array());
-            }
-            /** @var $response \OAuth2\Response */
-            $response = $oauth2->getServer()->getResponse();
-            if ($response->getStatusCode() !== 200) {
-                throw new CHttpException($response->getStatusCode(), $response->getParameter('error_description'));
-            }
-        }
-
-        parent::init();
-    }
 }
