e706a69cf178f2d74353d87707e409fde53548c0
Fredrik Wollsén <fredrik@neam.se>
2015-03-19 11:34:23 +0200

    Omitted port from API_HOST

--- tests/generate-local-codeception-config.sh
+++ tests/generate-local-codeception-config.sh
@@ -2,7 +2,7 @@
 
 script_path=`dirname $0`
 
-export API_HOST=web:80/api # <-- "web" docker virtual host since the api tests are done directly using curl
+export API_HOST=web/api # <-- "web" docker virtual host since the api tests are done directly using curl
 erb $script_path/codeception/api-v1.suite.yml.erb > $script_path/codeception/api-v1.suite.yml
 #erb $script_path/codeception/api-v2.suite.yml.erb > $script_path/codeception/api-v2.suite.yml
 
