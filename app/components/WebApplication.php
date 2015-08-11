2732c2d92fd69c4cd629c4476a8ab5e7a4eec6e1
Fredrik Wollsén <fredrik@neam.se>
2015-08-11 18:03:00 +0200

    Not sending allow-origin headers of origin header is not supplied

--- app/components/WebApplication.php
+++ app/components/WebApplication.php
@@ -11,17 +11,22 @@ class WebApplication extends CWebApplication
     public function sendCorsHeaders()
     {
 
-        $allowed_origins = explode(",", CORS_ACL_ORIGIN_HOSTS);
-        $http_origin = $_SERVER['HTTP_ORIGIN'];
-        $response_allowed_origin = $allowed_origins[0];
-        foreach ($allowed_origins as $allowed_origin) {
-            if (strpos($http_origin, "://" . $allowed_origin) !== false) {
-                $response_allowed_origin = $http_origin;
+        if (!empty($_SERVER['HTTP_ORIGIN'])) {
+
+            $allowed_origins = explode(",", CORS_ACL_ORIGIN_HOSTS);
+            $http_origin = $_SERVER['HTTP_ORIGIN'];
+            $response_allowed_origin = $allowed_origins[0];
+            foreach ($allowed_origins as $allowed_origin) {
+                if (strpos($http_origin, "://" . $allowed_origin) !== false) {
+                    $response_allowed_origin = $http_origin;
+                }
             }
+
+            header("Access-Control-Allow-Origin: " . $response_allowed_origin);
+            header("Access-Control-Allow-Credentials: true");
+
         }
 
-        header("Access-Control-Allow-Origin: " . $response_allowed_origin);
-        header("Access-Control-Allow-Credentials: true");
         header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
         header("Access-Control-Allow-Headers: Authorization, Origin, Content-Type, Accept");
     }
