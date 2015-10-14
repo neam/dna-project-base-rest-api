5114d91bb19089b11d8b77e4fac956271e53ddec
Fredrik Wollsén <fredrik@neam.se>
2015-10-14 07:40:28 +0300

    Somewhat more refined barebones exception response

--- www/index.php
+++ www/index.php
@@ -27,7 +27,14 @@ class BootstrapWebApplication
 
     public function displayException(Exception $e)
     {
-        echo json_encode(['status' => 'error', 'message' => $e->getMessage()]);
+        $response = [];
+        $response["status"] = ($e instanceof CHttpException) ? $e->statusCode : 500;
+        if (YII_DEBUG) {
+            $className = get_class($e);
+            $response["message"] =  "{$className}({$e->getCode()}): {$e->getMessage()} ({$e->getFile()}:{$e->getLine()})";
+            $response["trace"] = $e->getTrace();
+        }
+        echo json_encode($response);
     }
 }
 
